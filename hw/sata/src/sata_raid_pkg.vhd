0b4b51a5d31241eafe1715cb7e49d6b77e967240
vicg <vicg42@gmail.com>
2011-04-01 15:13:43 +0000

    SATA/RAID:
    git-svn-id: svn://10.1.7.240:3691/common@34 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ hw/sata/src/sata_raid_pkg.vhd
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------
+-- Company     : Linkos
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 01.04.2011 12:39:49
+-- Module Name : sata_raid_pkg
+--
+-- Description : Константы/Типы данных/
+--               используемые в sata_raid
+--
+-- Revision:
+-- Revision 0.01 - File Created
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
+
+use work.vicg_common_pkg.all;
+use work.sata_pkg.all;
+
+package sata_raid_pkg is
+
+
+--//
+type TUsrSErrorSataCountMax is array (0 to C_SATA_COUNT_MAX-1) of std_logic_vector(C_ALSERR_LAST_BIT downto 0);
+
+type TUsrStatus is record
+glob_busy: std_logic;
+glob_drdy: std_logic;
+glob_err : std_logic;
+ch_busy  : std_logic_vector(C_SATA_COUNT_MAX-1 downto 0);
+ch_drdy  : std_logic_vector(C_SATA_COUNT_MAX-1 downto 0);
+ch_err   : std_logic_vector(C_SATA_COUNT_MAX-1 downto 0);
+SError   : TUsrSErrorSataCountMax;
+glob_usr : std_logic_vector(31 downto 0);
+ch_usr   : TBus32_SataCountMax;
+end record;
+
+
+
+
+end sata_raid_pkg;
+
+
+package body sata_raid_pkg is
+
+---------------------------------------------------------
+--Функции
+---------------------------------------------------------
+
+
+end sata_raid_pkg;
+
+
