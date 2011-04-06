968f7c44f6fb54f73192b9f3910170271ed39c6c
vicg <vicg42@gmail.com>
2011-04-06 08:33:41 +0000

    В репозиторий veresk_m/common/ добавил
    git-svn-id: svn://10.1.7.240:3691/veresk_m@96 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ common/mirror/sim/testbanch/vmirx_main_tb.vhd
@@ -0,0 +1,357 @@
+-------------------------------------------------------------------------
+-- Company     : Linkos
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 25.11.2008 18:38
+-- Module Name : vscaler_main_tb
+--
+-- Назначение/Описание :
+--    Проверка работы
+--
+-- Revision:
+-- Revision 0.01 - File Created
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_textio.all;
+
+use work.vicg_common_pkg.all;
+
+library std;
+use std.textio.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity vmirx_main_tb is
+end vmirx_main_tb;
+
+architecture behavior of vmirx_main_tb is
+
+constant i_clk_period : TIME := 6.6 ns; --150MHz
+
+component vmirx_main
+port
+(
+-------------------------------
+-- Управление
+-------------------------------
+p_in_cfg_mirx              : in    std_logic;
+p_in_cfg_pix_count         : in    std_logic_vector(15 downto 0);
+
+p_out_cfg_mirx_done        : out   std_logic;
+
+--//--------------------------
+--//Upstream Port
+--//--------------------------
+--p_in_upp_clk               : in    std_logic;
+p_in_upp_data              : in    std_logic_vector(31 downto 0);
+p_in_upp_wd                : in    std_logic;
+p_out_upp_rdy_n            : out   std_logic;
+
+--//--------------------------
+--//Downstream Port
+--//--------------------------
+--p_in_dwnp_clk              : in    std_logic;
+p_out_dwnp_data            : out   std_logic_vector(31 downto 0);
+p_out_dwnp_wd              : out   std_logic;
+p_in_dwnp_rdy_n            : in    std_logic;
+
+-------------------------------
+--Технологический
+-------------------------------
+p_out_tst                  : out   std_logic_vector(7 downto 0);
+
+-------------------------------
+--System
+-------------------------------
+p_in_clk            : in    std_logic;
+p_in_rst            : in    std_logic
+);
+end component;
+
+component vmirx_fifo
+port (
+din        : IN  std_logic_VECTOR(31 downto 0);
+wr_en      : IN  std_logic;
+
+dout       : OUT std_logic_VECTOR(31 downto 0);
+rd_en      : IN  std_logic;
+
+empty      : OUT std_logic;
+full       : OUT std_logic;
+almost_full: OUT std_logic;
+
+clk        : IN  std_logic;
+rst        : IN  std_logic
+);
+end component;
+
+signal p_in_clk                       : std_logic := '0';
+signal p_in_rst                       : std_logic := '0';
+
+signal p_in_cfg_mirx                  : std_logic;
+signal p_in_cfg_pix_count             : std_logic_vector(15 downto 0);
+signal p_in_cfg_row_count             : std_logic_vector(15 downto 0);
+
+signal p_out_cfg_mirx_done            : std_logic;
+
+signal tst_data_out                   : std_logic_vector(31 downto 0);
+signal p_in_upp_wd                    : std_logic;
+signal p_out_upp_rdy_n                : std_logic;
+
+signal p_out_dwnp_data                : std_logic_vector(31 downto 0);
+signal p_out_dwnp_wd                  : std_logic;
+signal p_in_dwnp_rdy_n                : std_logic;
+
+signal tst_mnl_pix_count              : std_logic_vector(15 downto 0);
+signal tst_mnl_row_count              : std_logic_vector(15 downto 0);
+signal tst_mnl_puase_count            : std_logic_vector(15 downto 0);
+signal mnl_use_gen_dwnp_rdy           : std_logic;
+
+signal tst_stop                       : std_logic;
+signal tst_puse_count                 : std_logic_vector(15 downto 0);
+signal tst_row_count                  : std_logic_vector(15 downto 0);
+signal tst_pix_count                  : std_logic_vector(15 downto 0);
+signal tst_data                       : std_logic_vector(7 downto 0);
+signal mnl_write_testdata             : std_logic;
+
+signal mnl_dwnp_rdy_n                 : std_logic;
+signal i_dwnp_rdy_n                   : std_logic;
+
+signal i_fifo_result_dout             : std_logic_vector(31 downto 0);
+signal i_fifo_result_rd               : std_logic;
+signal i_fifo_result_empty            : std_logic;
+signal i_fifo_result_full             : std_logic;
+signal mnl_fifo_result_rd             : std_logic;
+
+signal tst_dwnp_count                 : std_logic_vector(31 downto 0);
+
+--Main
+begin
+
+
+
+m_vmirx: vmirx_main
+port map
+(
+-------------------------------
+-- Управление
+-------------------------------
+p_in_cfg_mirx              => p_in_cfg_mirx,
+p_in_cfg_pix_count         => p_in_cfg_pix_count,
+
+p_out_cfg_mirx_done        => p_out_cfg_mirx_done,
+
+--//--------------------------
+--//Upstream Port
+--//--------------------------
+p_in_upp_data              => tst_data_out,
+p_in_upp_wd                => p_in_upp_wd,
+p_out_upp_rdy_n            => p_out_upp_rdy_n,
+
+--//--------------------------
+--//Downstream Port
+--//--------------------------
+p_out_dwnp_data            => p_out_dwnp_data,
+p_out_dwnp_wd              => p_out_dwnp_wd,
+p_in_dwnp_rdy_n            => p_in_dwnp_rdy_n,
+
+-------------------------------
+--Технологический
+-------------------------------
+p_out_tst                  => open,
+
+-------------------------------
+--System
+-------------------------------
+p_in_clk            => p_in_clk,
+p_in_rst            => p_in_rst
+);
+
+m_fifo_result : vmirx_fifo
+port map
+(
+din         => p_out_dwnp_data,
+wr_en       => p_out_dwnp_wd,
+--wr_clk      => p_in_upp_clk,
+
+dout        => i_fifo_result_dout,
+rd_en       => i_fifo_result_rd,
+--rd_clk      => p_in_dwnp_clk,
+
+empty       => i_fifo_result_empty,
+full        => open,
+almost_full => i_fifo_result_full,
+
+clk         => p_in_clk,
+rst         => p_in_rst
+);
+
+clk_in_generator : process
+begin
+  p_in_clk<='0';
+  wait for i_clk_period/2;
+  p_in_clk<='1';
+  wait for i_clk_period/2;
+end process;
+
+
+p_in_rst<='1','0' after 1 us;
+
+--//----------------------------------------------------------
+--//Настройка тестирования
+--//----------------------------------------------------------
+p_in_cfg_mirx<='1';--//0/1 -Отзеркаливание по X/ Bypass
+
+--//Конфигурируем генератор тестровых данных:
+p_in_cfg_pix_count<=CONV_STD_LOGIC_VECTOR(16#024#, p_in_cfg_pix_count'length); --
+p_in_cfg_row_count<=CONV_STD_LOGIC_VECTOR(16#03#, p_in_cfg_row_count'length); --
+tst_mnl_puase_count<=CONV_STD_LOGIC_VECTOR(16#82#, tst_mnl_puase_count'length); --//Пауза между строками
+
+--// 1/0 Генерировать/НЕ Гненерировать waveform для сигнала p_in_dwnp_rdy_n
+mnl_use_gen_dwnp_rdy<='1';
+
+
+
+--//----------------------------------------------------------
+--//
+--//----------------------------------------------------------
+p_in_dwnp_rdy_n<=i_dwnp_rdy_n when mnl_use_gen_dwnp_rdy='1' else '0';
+
+
+tst_mnl_pix_count  <=p_in_cfg_pix_count;   --//Кол-во пикселей
+tst_mnl_row_count  <=p_in_cfg_row_count;   --//Кол-во строк
+
+
+mnl_write_testdata<='0','1' after 2.5 us;
+
+
+mnl_dwnp_rdy_n<='0',
+                '1' after 2.5 us, '0' after 2.5654 us,
+                '1' after 2.58 us, '1' after 2.60987 us,
+                '1' after 2.6134 us, '0' after 2.677 us,
+                '1' after 2.78 us, '0' after 2.84 us,
+                '1' after 2.85 us, '0' after 2.86 us,
+                '1' after 2.8678 us, '0' after 2.887 us,
+                '1' after 2.89 us, '0' after 2.987 us,
+                '1' after 3.00 us,'0' after 3.126 us,
+                '1' after 4.0 us, '0' after 4.0678 us;
+
+mnl_fifo_result_rd<='0',
+                     '1' after 3.1 us, '0' after 3.15654 us,
+                     '1' after 3.285 us, '1' after 3.2987 us,
+                     '1' after 3.8 us, '0' after 3.987 us,
+                     '1' after 4.0 us, '0' after 4.1234 us;
+
+--//Генератор тестовых данных
+process(p_in_rst,p_in_clk)
+  variable GUI_line : LINE;--Строка дл_ вывода в ModelSim
+begin
+  if p_in_rst='1' then
+    tst_stop<='0';
+    tst_row_count<=(others=>'0');
+    tst_puse_count<=(others=>'0');
+    tst_pix_count<=(others=>'0');
+    tst_data<=CONV_STD_LOGIC_VECTOR(16#2#, tst_data'length); --//
+    tst_data_out<=(others=>'0');
+    p_in_upp_wd<='0';
+    i_dwnp_rdy_n<='0';
+    i_fifo_result_rd<='0';
+  elsif p_in_clk'event and p_in_clk='1' then
+    i_dwnp_rdy_n<=mnl_dwnp_rdy_n;
+
+    i_fifo_result_rd<=mnl_fifo_result_rd and not i_fifo_result_empty;
+
+    if mnl_write_testdata='1' then
+
+      if i_fifo_result_full='0' then
+
+        if tst_pix_count=tst_mnl_pix_count then
+--          tst_data<=CONV_STD_LOGIC_VECTOR(16#0#, tst_data'length); --//
+
+          if tst_row_count=tst_mnl_row_count-1 then
+            p_in_upp_wd<='0';
+          else
+            if tst_puse_count=tst_mnl_puase_count then
+              tst_row_count<=tst_row_count+1;
+              tst_pix_count<=(others=>'0');
+              tst_puse_count<=(others=>'0');
+            else
+              p_in_upp_wd<='0';
+              tst_puse_count<=tst_puse_count+1;
+            end if;
+          end if;
+
+        else
+          tst_pix_count<=tst_pix_count+1;
+          p_in_upp_wd<='1';
+
+--          tst_data<=tst_data+4;
+--          tst_data_out(7 downto 0)  <=tst_data;
+--          tst_data_out(15 downto 8) <=tst_data+1;
+--          tst_data_out(23 downto 16)<=tst_data+2;
+--          tst_data_out(31 downto 24)<=tst_data+3;
+
+          tst_data<=tst_data+8;
+          tst_data_out(7 downto 0)  <=tst_data;
+          tst_data_out(15 downto 8) <=tst_data+2;
+          tst_data_out(23 downto 16)<=tst_data+4;
+          tst_data_out(31 downto 24)<=tst_data+6;
+
+--          write(GUI_line, string'("TEST DATA 2DW ("));
+--          hwrite(GUI_line, tst_dwnp_count);
+--          write(GUI_line, string'(") : 0x"));
+--          hwrite(GUI_line, tst_data_out);
+--          writeline(output, GUI_line);--Выводим строку GUI_line в ModelSim
+
+        end if;
+      else
+        p_in_upp_wd<='0';
+      end if;
+    else
+      p_in_upp_wd<='0';
+    end if;
+  end if;
+end process;
+
+
+
+process(p_in_rst,p_in_clk)
+  variable GUI_line : LINE;--Строка дл_ вывода в ModelSim
+begin
+  if p_in_rst='1' then
+    tst_dwnp_count<=(others=>'0');
+  elsif p_in_clk'event and p_in_clk='1' then
+
+--    if p_out_dwnp_wd='1' then
+--        tst_dwnp_count<=tst_dwnp_count+1;
+--
+--        write(GUI_line, string'("Result 2DW ("));
+--        hwrite(GUI_line, tst_dwnp_count);
+--        write(GUI_line, string'(") : 0x"));
+--        hwrite(GUI_line, p_out_dwnp_data);
+--        writeline(output, GUI_line);--Выводим строку GUI_line в ModelSim
+--
+--    end if;
+
+    if i_fifo_result_rd='1' then
+        tst_dwnp_count<=tst_dwnp_count+1;
+
+        write(GUI_line, string'("Result 2DW ("));
+        hwrite(GUI_line, tst_dwnp_count);
+        write(GUI_line, string'(") : 0x"));
+        hwrite(GUI_line, i_fifo_result_dout);
+        writeline(output, GUI_line);--Выводим строку GUI_line в ModelSim
+
+    end if;
+
+  end if;
+end process;
+
+--End Main
+end;
---
+++ common/mirror/vmirx_main.vhd
@@ -0,0 +1,310 @@
+-------------------------------------------------------------------------
+-- Company     : Linkos
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 2010.07
+-- Module Name : vmirx_main
+--
+-- Назначение/Описание :
+--  Модуль реализует отзеркаливание строки видео инфомации по оси X
+--
+--  Если отзеркаливание ВЫКЛ. то всеравно выдача данных в выходной порт
+--  идет через внутренний промежуточный буфер (m_bufline)
+--
+--  Натройка работы модуля:
+--  1. Выбрать режим работы модуля. Порт p_in_cfg_mirx - 0/1:
+--     Вкл./Выкл отзеркаливание строки видеоданных.
+--  2. Назначить размер входного кадра порт p_in_cfg_pix_count
+--
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Revision 0.02 - --//add 19.01.2011 10:29:40
+--                 ввел сигнал i_read_en для исправление колиизии BRAM
+--                 collision detected: A write address: 0000000101, B read address: 0000000101
+--                 Обнаружено при моделировании.
+--                 + Легкие причесывания
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_misc.all;
+use ieee.std_logic_unsigned.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+library work;
+use work.vicg_common_pkg.all;
+--use work.prj_def.all;
+
+entity vmirx_main is
+port
+(
+-------------------------------
+-- Управление
+-------------------------------
+p_in_cfg_mirx              : in    std_logic;                    --//1/0 - Вкл./Выкл отзеркаливание строки видеоданных
+p_in_cfg_pix_count         : in    std_logic_vector(15 downto 0);--//Кол-во пиксел/4 т.к p_in_upp_data=32bit
+
+p_out_cfg_mirx_done        : out   std_logic;                    --//Обработка завершена.
+
+--//--------------------------
+--//Upstream Port (входные данные)
+--//--------------------------
+--p_in_upp_clk               : in    std_logic;
+p_in_upp_data              : in    std_logic_vector(31 downto 0);
+p_in_upp_wd                : in    std_logic;                    --//Запись данных в модуль vmirx_main.vhd
+p_out_upp_rdy_n            : out   std_logic;                    --//0 - Модуль vmirx_main.vhd готов к приему данных
+
+--//--------------------------
+--//Downstream Port (результат)
+--//--------------------------
+--p_in_dwnp_clk              : in    std_logic;
+p_out_dwnp_data            : out   std_logic_vector(31 downto 0);
+p_out_dwnp_wd              : out   std_logic;                    --//Запись данных в приемник
+p_in_dwnp_rdy_n            : in    std_logic;                    --//0 - порт приемника готов к приему даннвх
+
+-------------------------------
+--Технологический
+-------------------------------
+p_in_tst                   : in    std_logic_vector(31 downto 0);
+p_out_tst                  : out   std_logic_vector(31 downto 0);
+
+-------------------------------
+--System
+-------------------------------
+p_in_clk            : in    std_logic;
+p_in_rst            : in    std_logic
+);
+end vmirx_main;
+
+architecture behavioral of vmirx_main is
+
+constant dly : time := 1 ps;
+
+component vmirx_bram
+port
+(
+addra: IN  std_logic_VECTOR(9 downto 0);
+dina : IN  std_logic_VECTOR(31 downto 0);
+douta: OUT std_logic_VECTOR(31 downto 0);
+ena  : IN  std_logic;
+wea  : IN  std_logic_VECTOR(0 downto 0);
+clka : IN  std_logic;
+rsta : IN  std_logic;
+
+addrb: IN  std_logic_VECTOR(9 downto 0);
+dinb : IN  std_logic_VECTOR(31 downto 0);
+doutb: OUT std_logic_VECTOR(31 downto 0);
+enb  : IN  std_logic;
+web  : IN  std_logic_VECTOR(0 downto 0);
+clkb : IN  std_logic;
+rstb : IN  std_logic
+);
+end component;
+
+
+signal i_upp_data                        : std_logic_vector(31 downto 0);
+signal i_upp_data_swap                   : std_logic_vector(31 downto 0);
+signal i_upp_wd                          : std_logic;
+
+type fsm_state is
+(
+  S_WRITE_BUFLINE,
+  S_READ_BUFLINE_SOF,
+  S_READ_BUFLINE,
+  S_READ_BUFLINE_EOF
+);
+signal fsm_state_cs: fsm_state;
+
+signal i_pix_count                       : std_logic_vector(p_in_cfg_pix_count'high downto 0);
+signal i_mirx_done                       : std_logic;
+
+signal i_tmpbuf_addra                    : std_logic_vector(i_pix_count'range);--(9 downto 0);
+signal i_tmpbuf_din                      : std_logic_vector(31 downto 0);
+signal i_tmpbuf_dout                     : std_logic_vector(31 downto 0);
+signal i_tmpbuf_dir                      : std_logic;
+signal i_tmpbuf_ena                      : std_logic;
+signal i_tmpbuf_enb                      : std_logic;
+signal i_read_en                         : std_logic;--//add 19.01.2011 10:29:40
+
+
+--MAIN
+begin
+
+
+--//----------------------------------
+--//Технологические сигналы
+--//----------------------------------
+--process(p_in_rst,p_in_clk)
+--begin
+--  if p_in_rst='1' then
+--    p_out_tst(31 downto 0)<=(others=>'0');
+--
+--  elsif p_in_clk'event and p_in_clk='1' then
+--
+--    p_out_tst(0)<=OR_reduce(i_zoom_work_done_dly) or OR_reduce(i_lbufs_dout(0)) or i_lbufs_dout_en;
+--
+--  end if;
+--end process;
+p_out_tst(31 downto 0)<=(others=>'0');
+
+
+--//----------------------------------------------
+--//Связь с Upstream Port
+--//----------------------------------------------
+i_upp_data <=p_in_upp_data;
+i_upp_wd   <=p_in_upp_wd;
+
+p_out_upp_rdy_n <=i_tmpbuf_dir;
+
+--//-----------------------------
+--//Вывод результата
+--//-----------------------------
+p_out_dwnp_data <=i_tmpbuf_dout;
+p_out_dwnp_wd   <=not p_in_dwnp_rdy_n and i_tmpbuf_dir;
+
+
+--//-----------------------------
+--//Инициализация
+--//-----------------------------
+i_pix_count<=p_in_cfg_pix_count;
+
+
+--//-----------------------------
+--//Статус
+--//-----------------------------
+p_out_cfg_mirx_done <=i_mirx_done;
+
+
+--//------------------------------------
+--//Запись/Чтение Буфера строки
+--//------------------------------------
+process(p_in_rst,p_in_clk)
+begin
+  if p_in_rst='1' then
+
+    fsm_state_cs <= S_WRITE_BUFLINE;
+
+    i_tmpbuf_dir<='0';
+    i_tmpbuf_addra<=(others=>'0');
+    i_mirx_done<='1';
+    i_read_en<='0';--//add 19.01.2011 10:29:40
+
+  elsif p_in_clk'event and p_in_clk='1' then
+
+    case fsm_state_cs is
+
+      --//------------------------------------
+      --//Запись данных строки в буфер
+      --//------------------------------------
+      when S_WRITE_BUFLINE =>
+        i_mirx_done<='0';
+
+        if i_upp_wd='1' then
+          if i_tmpbuf_addra=i_pix_count-1 then
+            if p_in_cfg_mirx='0' then
+              i_tmpbuf_addra<=(others=>'0');
+            end if;
+            i_read_en<='1';--//add 19.01.2011 10:29:40
+
+            fsm_state_cs <= S_READ_BUFLINE_SOF;
+          else
+            i_tmpbuf_addra<=i_tmpbuf_addra+1;
+          end if;
+        end if;
+
+      --//------------------------------------
+      --//
+      --//------------------------------------
+      when S_READ_BUFLINE_SOF =>
+        i_tmpbuf_dir<='1';--//Переключаемся на чтение m_row_buf
+
+        if p_in_cfg_mirx='0' then
+          i_tmpbuf_addra<=i_tmpbuf_addra+1;
+        else
+          i_tmpbuf_addra<=i_tmpbuf_addra-1;
+        end if;
+        fsm_state_cs <= S_READ_BUFLINE;
+
+      --//------------------------------------
+      --//Чтение данных из буфера строки
+      --//------------------------------------
+      when S_READ_BUFLINE =>
+
+        if p_in_dwnp_rdy_n='0' then
+
+            --//Отзеркаливание по Х: ЕСТЬ
+            if (p_in_cfg_mirx='0' and i_tmpbuf_addra=i_pix_count-1) or
+               (p_in_cfg_mirx='1' and i_tmpbuf_addra=(i_tmpbuf_addra'range => '0')) then
+
+              fsm_state_cs <=S_READ_BUFLINE_EOF;
+            else
+              if p_in_cfg_mirx='0' then
+                i_tmpbuf_addra<=i_tmpbuf_addra+1;
+              else
+                i_tmpbuf_addra<=i_tmpbuf_addra-1;
+              end if;
+            end if;
+
+        end if;
+
+      --//------------------------------------
+      --//
+      --//------------------------------------
+      when S_READ_BUFLINE_EOF =>
+        if p_in_dwnp_rdy_n='0' then
+          i_mirx_done<='1';
+          i_tmpbuf_dir<='0';--/Переключаемся на Запись m_row_buf
+          i_read_en<='0';--//add 19.01.2011 10:29:40
+          if p_in_cfg_mirx='0' then
+            i_tmpbuf_addra<=(others=>'0');
+          end if;
+          fsm_state_cs <= S_WRITE_BUFLINE;
+        end if;
+    end case;
+
+  end if;
+end process;
+
+
+--//Если Отзеркаливание ЕСТЬ, то для 1Pix=8Bit
+i_upp_data_swap(7 downto 0)  <=i_upp_data(31 downto 24);
+i_upp_data_swap(15 downto 8) <=i_upp_data(23 downto 16);
+i_upp_data_swap(23 downto 16)<=i_upp_data(15 downto 8);
+i_upp_data_swap(31 downto 24)<=i_upp_data(7 downto 0);
+
+--//Запись данных
+i_tmpbuf_din<=i_upp_data_swap when p_in_cfg_mirx='1' else i_upp_data;
+i_tmpbuf_ena<=not i_tmpbuf_dir and i_upp_wd;
+
+--//Чтение данных
+i_tmpbuf_enb<=(not p_in_dwnp_rdy_n or not i_tmpbuf_dir) and i_read_en;--//add 19.01.2011 10:29:40
+
+--//БУФЕР НА СТРОКУ
+m_bufline : vmirx_bram
+port map
+(
+addra => i_tmpbuf_addra(9 downto 0),
+dina  => i_tmpbuf_din,
+douta => open,
+ena   => i_tmpbuf_ena,
+wea   => "1",
+clka  => p_in_clk,
+rsta  => p_in_rst,
+
+addrb => i_tmpbuf_addra(9 downto 0),
+dinb  => "00000000000000000000000000000000",
+doutb => i_tmpbuf_dout,
+enb   => i_tmpbuf_enb,
+web   => "0",
+clkb  => p_in_clk,
+rstb  => p_in_rst
+);
+
+
+--END MAIN
+end behavioral;
+
