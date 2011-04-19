1c8781a844f3e372384ee523cdb80dd535ff2661
vicg <vicg42@gmail.com>
2011-04-19 12:23:06 +0000

    vpcolor_main.vhd - откат к rev 0.02
    git-svn-id: svn://10.1.7.240:3691/common@113 244e6edd-17f5-4270-a3ef-e79531b98298

--- hw/video/pcolor/vpcolor_main.vhd
+++ hw/video/pcolor/vpcolor_main.vhd
@@ -28,14 +28,10 @@
 -- Revision 0.01 - File Created
 -- Revision 0.02 - add 10.02.2011 15:42:10
 --                 Поменял порядок цветовых каналов в выходных данных, потому что было неправильно
---                 Теперь:                                         а раньше было:
---                    (7..0)  =B                                   (7..0)  =R
---                    (15..8) =G                                   (15..8) =G
---                    (23..16)=R                                   (23..16)=B
---                    (31..24)=0xFF (прозрачность - альфа канал)   (31..24)=0xFF
---
--- Revision 1.00 - add 11.02.2011 18:58:37
---                 Переделал обработку входных данных.
+--                 Теперь:(7..0)  =B                                а раньше было:(7..0)  =R
+--                        (15..8) =G                                              (15..8) =G
+--                        (23..16)=R                                              (23..16)=B
+--                        (31..24)=0xFF (прозрачность - альфа канал)              (31..24)=0xFF (прозрачность - альфа канал)
 -------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -45,6 +41,9 @@ use ieee.std_logic_unsigned.all;
 
 library unisim;
 use unisim.vcomponents.all;
+
+--library work;
+--use work.prj_def.all;
 
 entity vpcolor_main is
 port
@@ -157,27 +156,52 @@ rstb : in  std_logic
 );
 end component;
 
+component vpcolor_fifo
+port (
+din        : in  std_logic_vector(31 downto 0);
+wr_en      : in  std_logic;
 
-signal i_upp_rdy_n_out                   : std_logic;
+dout       : out std_logic_vector(31 downto 0);
+rd_en      : in  std_logic;
 
-signal sr_upp_data                       : std_logic_vector(31 downto 0);
+empty      : out std_logic;
+full       : out std_logic;
+almost_full: out std_logic;
 
-signal i_byte_cnt_init                   : std_logic_vector(1 downto 0);
+clk        : in  std_logic;
+rst        : in  std_logic
+);
+end component;
+
+signal i_dwnp_rdy_n                      : std_logic;
+
+signal i_fifo_empty                      : std_logic;
+signal i_fifo_dout                       : std_logic_vector(31 downto 0);
+signal i_fifo_rd                         : std_logic;
+signal i_fsm_fifo_rd                     : std_logic;
+signal i_fifo_read_en_n                  : std_logic;
+
+type fsm_state is
+(
+S_READ_INDATA,
+S_CONVERTION
+);
+signal fsm_state_cs: fsm_state;
+
+type TArrayInDW is array (3 downto 0) of std_logic_vector(7 downto 0);
+signal sr_indw                           : TArrayInDW;
+
 signal i_byte_cnt                        : std_logic_vector(1 downto 0);
 
-signal i_result_out                      : std_logic_vector(31 downto 0);
-signal i_result_en_out                   : std_logic;
-signal sr_result_en                      : std_logic_vector(0 to 0);
-
-signal i_bram_read                       : std_logic;
-signal i_r_dout                          : std_logic_vector(7 downto 0);
-signal i_g_dout                          : std_logic_vector(7 downto 0);
-signal i_b_dout                          : std_logic_vector(7 downto 0);
-
-signal i_coebuf_awrite                   : std_logic_vector(6 downto 0);
 signal i_wr_rbuf                         : std_logic_vector(0 downto 0);
 signal i_wr_gbuf                         : std_logic_vector(0 downto 0);
 signal i_wr_bbuf                         : std_logic_vector(0 downto 0);
+signal i_result_out                      : std_logic_vector(31 downto 0);
+signal i_result_out_en                   : std_logic;
+signal i_result_out_en_dly               : std_logic;
+signal i_bram_rd_en                      : std_logic;
+
+signal i_coebuf_awrite                   : std_logic_vector(6 downto 0);
 signal i_rcoe_dout                       : std_logic_vector(15 downto 0);
 signal i_gcoe_dout                       : std_logic_vector(15 downto 0);
 signal i_bcoe_dout                       : std_logic_vector(15 downto 0);
@@ -198,98 +222,128 @@ begin
 --
 --  elsif p_in_clk'event and p_in_clk='1' then
 --
---    p_out_tst(0)<=;
+--    p_out_tst(0)<=OR_reduce(i_zoom_work_done_dly) or OR_reduce(i_lbufs_dout(0)) or i_lbufs_dout_en;
 --
 --  end if;
 --end process;
 p_out_tst(31 downto 0)<=(others=>'0');
 
 
-
 --//-----------------------------
---//Инициализация
+--//Входной буфер
 --//-----------------------------
-i_byte_cnt_init<="11";
+m_fifo_in : vpcolor_fifo
+port map
+(
+din         => p_in_upp_data,
+wr_en       => p_in_upp_wd,
+--wr_clk      => p_in_upp_clk,
 
+dout        => i_fifo_dout,
+rd_en       => i_fifo_rd,
+--rd_clk      => p_in_dwnp_clk,
 
---//----------------------------------------------
---//Связь с Upstream Port
---//----------------------------------------------
-p_out_upp_rdy_n <=p_in_dwnp_rdy_n or i_upp_rdy_n_out;
+empty       => i_fifo_empty,
+full        => open,
+almost_full => p_out_upp_rdy_n,
+
+clk         => p_in_clk,
+rst         => p_in_rst
+);
+
+i_fifo_rd <=i_fsm_fifo_rd when p_in_cfg_bypass='0' else not i_fifo_empty and not p_in_dwnp_rdy_n;
 
 
 --//-----------------------------
 --//Вывод результата
 --//-----------------------------
-p_out_dwnp_data <=i_result_out    when p_in_cfg_bypass='0' else p_in_upp_data;
-p_out_dwnp_wd   <=i_result_en_out when p_in_cfg_bypass='0' else p_in_upp_wd;
+p_out_dwnp_data <=i_result_out                                when p_in_cfg_bypass='0' else i_fifo_dout;
+p_out_dwnp_wd   <=i_result_out_en_dly and not p_in_dwnp_rdy_n when p_in_cfg_bypass='0' else i_fifo_rd;
 
 
+
+--//-----------------------------
+--//Инициализация
+--//-----------------------------
+
+i_dwnp_rdy_n<=p_in_dwnp_rdy_n or p_in_cfg_bypass;
+i_bram_rd_en<=not i_dwnp_rdy_n and i_result_out_en;
+
 --//------------------------------------
---//Управление чтением входных данных
+--//Автомат:
+--//Управление чтением входного буфера/ записью/чтением буферов строк/ чтением коэфициентов
 --//------------------------------------
+i_fsm_fifo_rd<=not i_fifo_empty and not i_fifo_read_en_n and not i_dwnp_rdy_n;
+
+--//Логика работы автомата
 process(p_in_rst,p_in_clk)
 begin
   if p_in_rst='1' then
-    i_upp_rdy_n_out<='0';
 
+    fsm_state_cs <= S_READ_INDATA;
+
+    i_fifo_read_en_n<='0';
+    i_result_out_en<='0';
+    for i in 0 to 3 loop
+      sr_indw(i)<=(others=>'0');
+    end loop;
     i_byte_cnt<=(others=>'0');
-    sr_upp_data<=(others=>'0');
 
   elsif p_in_clk'event and p_in_clk='1' then
+  if i_dwnp_rdy_n='0' then
 
-    if p_in_cfg_bypass='1' then
-      i_upp_rdy_n_out<='0';
+    case fsm_state_cs is
 
-    else
-        --//Обработка кадра ВКЛ.
-        if p_in_dwnp_rdy_n='0' then
+      --//------------------------------------
+      --//Запись данных строки в буфер
+      --//------------------------------------
+      when S_READ_INDATA =>
+        i_result_out_en<='0';
 
-            --//-----------------------------
-            --//Обработка входных данных:
-            --//-----------------------------
-              if p_in_upp_wd='1' then
-                --//Прием нового входного DWORD
-                i_upp_rdy_n_out<='1';--//Запрещаем запись входных данных в буфер строки на
-                                     --//время обработки всех байт входного DWORD
+        if i_fifo_empty='0' then
+          if i_fifo_read_en_n='0' then
+              i_fifo_read_en_n<='1';--//ЗАПРЕЩАЕМ чтение данных.
+              i_result_out_en<='1';
 
-                sr_upp_data<=p_in_upp_data;
+              --//Готовим данные для сдвигового регистра
+              for i in 0 to 3 loop
+                sr_indw(i)<=i_fifo_dout(8*(i+1)-1 downto 8*i);
+              end loop;
 
-              else
-                  if i_upp_rdy_n_out='1' then
-                  --//Обработка байт входного DWORD
-                    if i_byte_cnt=i_byte_cnt_init then
-                      i_byte_cnt<=(others=>'0');
-                      i_upp_rdy_n_out<='0';
-                    else
-                      i_byte_cnt<=i_byte_cnt+1;--//Ведем подсчет байт входного DWORD
-                    end if;
+              i_byte_cnt<=CONV_STD_LOGIC_VECTOR(10#3#, i_byte_cnt'length);
+              fsm_state_cs <= S_CONVERTION;
+           end if;
+        end if;
 
-                    sr_upp_data<="00000000"&sr_upp_data(31 downto 8);
-                  end if;--//if i_upp_rdy_n_out='1'
-              end if;--//if p_in_upp_wd='1' then
+      --//------------------------------------
+      --//Обработка данных записаных в буфер
+      --//------------------------------------
+      when S_CONVERTION =>
 
+        if i_byte_cnt=(i_byte_cnt'range=>'0') then
+          i_fifo_read_en_n<='0';--//РАЗРЕШАЕМ чтение данных.
+          i_result_out_en<='0';
+          fsm_state_cs <= S_READ_INDATA;
+        else
+          i_byte_cnt<=i_byte_cnt-1;
+        end if;
 
-        end if;--//if p_in_dwnp_rdy_n='0' then
-    end if;--//if p_in_cfg_bypass='1'then
+        --//Сдвигавй регистр входных данных
+        sr_indw<="00000000"&sr_indw(3 downto 1);
+
+    end case;
+  end if;--  //if i_dwnp_rdy_n='0' then
   end if;
 end process;
 
---//------------------------------------------------------
---//Линии задержек
---//------------------------------------------------------
 process(p_in_rst,p_in_clk)
 begin
   if p_in_rst='1' then
-
-    sr_result_en<=(others=>'0');
-
+    i_result_out_en_dly<='0';
   elsif p_in_clk'event and p_in_clk='1' then
-    if p_in_dwnp_rdy_n='0' then
-
-        sr_result_en(0)<=i_upp_rdy_n_out;
-
-    end if;--//if p_in_dwnp_rdy_n='0' then
+  if i_dwnp_rdy_n='0' then
+      i_result_out_en_dly<=i_result_out_en;
+  end if;--  //if i_dwnp_rdy_n='0' then
   end if;
 end process;
 
@@ -297,10 +351,7 @@ end process;
 --//------------------------------------------------------
 --//RAM
 --//------------------------------------------------------
---//Чтение данных BRAM
-i_bram_read<=not p_in_dwnp_rdy_n and i_upp_rdy_n_out;
-
---//Запись данных в BRAM
+--//Запись данных в буфера строк
 process(p_in_rst,p_in_cfg_coe_wrclk)
 begin
   if p_in_rst='1' then
@@ -323,6 +374,7 @@ p_out_cfg_dcoe<=i_rcoe_dout when p_in_cfg_coeram_num="00" else
                 i_gcoe_dout when p_in_cfg_coeram_num="01" else
                 i_bcoe_dout;
 
+
 m_read_bram : vpcolor_rbram
 port map
 (
@@ -336,10 +388,10 @@ clka => p_in_cfg_coe_wrclk,
 rsta => p_in_rst,
 
 --//чтние
-addrb=> sr_upp_data(7 downto 0),
+addrb=> sr_indw(0),
 dinb => "00000000",
-doutb=> i_r_dout,
-enb  => i_bram_read,
+doutb=> i_result_out(23 downto 16),
+enb  => i_bram_rd_en,
 web  => "0",
 clkb => p_in_clk,
 rstb => p_in_rst
@@ -358,10 +410,10 @@ clka => p_in_cfg_coe_wrclk,
 rsta => p_in_rst,
 
 --//чтние
-addrb=> sr_upp_data(7 downto 0),
+addrb=> sr_indw(0),
 dinb => "00000000",
-doutb=> i_g_dout,
-enb  => i_bram_read,
+doutb=> i_result_out(15 downto 8),
+enb  => i_bram_rd_en,
 web  => "0",
 clkb => p_in_clk,
 rstb => p_in_rst
@@ -380,21 +432,16 @@ clka => p_in_cfg_coe_wrclk,
 rsta => p_in_rst,
 
 --//чтние
-addrb=> sr_upp_data(7 downto 0),
+addrb=> sr_indw(0),
 dinb => "00000000",
-doutb=> i_b_dout,
-enb  => i_bram_read,
+doutb=> i_result_out(7 downto 0),
+enb  => i_bram_rd_en,
 web  => "0",
 clkb => p_in_clk,
 rstb => p_in_rst
 );
 
---//Формирую результат:
---//COLOR:
---//           ALPHA     |  RED     |  GREEN   |  BLUE   |
-i_result_out<="11111111" & i_r_dout & i_g_dout & i_b_dout;
-
-i_result_en_out<=sr_result_en(0) and not p_in_dwnp_rdy_n;
+i_result_out(31 downto 24)<=(others=>'1');
 
 --END MAIN
 end behavioral;
