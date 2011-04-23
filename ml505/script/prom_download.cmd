db2798b24584dc83d825a4cef8923b3f0d9e91fe
vicg <vicg42@gmail.com>
2011-04-23 11:10:52 +0000

    firmware 030A - ml505 - корректировка ucf, script + rocketio_wrapper_gtp_tile.vhd
    git-svn-id: svn://10.1.7.240:3691/veresk_m@131 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/script/prom_download.cmd
+++ ml505/script/prom_download.cmd
@@ -3,12 +3,12 @@ setSubmode -pffserial
 addPromDevice -p 1 -name xcf32p
 addDesign -version 0 -name 0
 addDeviceChain -index 0
-addDevice -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\rrrrr.bit
-generate -compressed -format mcs -fillvalue FF -output D:\Work\Linkos\veresk_m\ml505\firmware\rrrr.mcs
+addDevice -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_main.bit
+generate -format mcs -fillvalue FF -output D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_main.mcs
 setMode -bs
 setCable -port auto
 identify
-assignFile -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\rrrr.mcs
+assignFile -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_main.mcs
 setAttribute -position 1 -attr readnextdevice -value "(null)"
 Program -p 1 -e -parallel
 quit
