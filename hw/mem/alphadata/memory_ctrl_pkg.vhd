08c409fdb352dcdcd1abafe77f3d44c91febff9f
vicg <vicg42@gmail.com>
2011-05-29 14:37:22 +0000

    чистка
    git-svn-id: svn://10.1.7.240:3691/common@223 244e6edd-17f5-4270-a3ef-e79531b98298

--- hw/mem/alphadata/memory_ctrl_pkg.vhd
+++ hw/mem/alphadata/memory_ctrl_pkg.vhd
@@ -105,59 +105,61 @@ port
 -------------------------------
 -- Конфигурирование
 -------------------------------
-p_in_cfg_mem_adr           : in    std_logic_vector(31 downto 0);--//Адрес ОЗУ (в BYTE)
-p_in_cfg_mem_trn_len       : in    std_logic_vector(15 downto 0);--//Размер одиночной mem_trn
-p_in_cfg_mem_dlen_rq       : in    std_logic_vector(15 downto 0);--//Размер запрашиваемых данных записи/чтения (в DWORD)
-p_in_cfg_mem_wr            : in    std_logic;                    --//Тип операции (1/0 - запись/чтение)
-p_in_cfg_mem_start         : in    std_logic;                    --//Пуск операции
-p_out_cfg_mem_done         : out   std_logic;                    --//Завершение операции
+p_in_cfg_mem_adr     : in    std_logic_vector(31 downto 0);
+p_in_cfg_mem_trn_len : in    std_logic_vector(15 downto 0);
+p_in_cfg_mem_dlen_rq : in    std_logic_vector(15 downto 0);
+p_in_cfg_mem_wr      : in    std_logic;
+p_in_cfg_mem_start   : in    std_logic;
+p_out_cfg_mem_done   : out   std_logic;
 
 -------------------------------
 -- Связь с пользовательскими буферами
 -------------------------------
-p_in_usr_txbuf_dout        : in    std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
-p_out_usr_txbuf_rd         : out   std_logic;
-p_in_usr_txbuf_empty       : in    std_logic;
+--//usr_buf->mem
+p_in_usr_txbuf_dout  : in    std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
+p_out_usr_txbuf_rd   : out   std_logic;
+p_in_usr_txbuf_empty : in    std_logic;
 
-p_out_usr_rxbuf_din        : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
-p_out_usr_rxbuf_wd         : out   std_logic;
-p_in_usr_rxbuf_full        : in    std_logic;
+--//usr_buf<-mem
+p_out_usr_rxbuf_din  : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
+p_out_usr_rxbuf_wd   : out   std_logic;
+p_in_usr_rxbuf_full  : in    std_logic;
 
 ---------------------------------
 -- Связь с memory_ctrl.vhd
 ---------------------------------
-p_out_memarb_req           : out   std_logic;
-p_in_memarb_en             : in    std_logic;
+p_out_memarb_req     : out   std_logic;
+p_in_memarb_en       : in    std_logic;
 
-p_out_mem_bank1h           : out   std_logic_vector(15 downto 0);
-p_out_mem_ce               : out   std_logic;
-p_out_mem_cw               : out   std_logic;
-p_out_mem_rd               : out   std_logic;
-p_out_mem_wr               : out   std_logic;
-p_out_mem_term             : out   std_logic;
-p_out_mem_adr              : out   std_logic_vector(C_MEMCTRL_ADDR_WIDTH - 1 downto 0);
-p_out_mem_be               : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH / 8 - 1 downto 0);
-p_out_mem_din              : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
-p_in_mem_dout              : in    std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
+p_out_mem_bank1h     : out   std_logic_vector(15 downto 0);
+p_out_mem_ce         : out   std_logic;
+p_out_mem_cw         : out   std_logic;
+p_out_mem_rd         : out   std_logic;
+p_out_mem_wr         : out   std_logic;
+p_out_mem_term       : out   std_logic;
+p_out_mem_adr        : out   std_logic_vector(C_MEMCTRL_ADDR_WIDTH - 1 downto 0);
+p_out_mem_be         : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH / 8 - 1 downto 0);
+p_out_mem_din        : out   std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
+p_in_mem_dout        : in    std_logic_vector(C_MEMCTRL_DATA_WIDTH - 1 downto 0);
 
-p_in_mem_wf                : in    std_logic;
-p_in_mem_wpf               : in    std_logic;
-p_in_mem_re                : in    std_logic;
-p_in_mem_rpe               : in    std_logic;
+p_in_mem_wf          : in    std_logic;
+p_in_mem_wpf         : in    std_logic;
+p_in_mem_re          : in    std_logic;
+p_in_mem_rpe         : in    std_logic;
 
-p_out_mem_clk              : out   std_logic;
+p_out_mem_clk        : out   std_logic;
 
 -------------------------------
---Технологический
+--Технологические сигналы
 -------------------------------
-p_in_tst            : in    std_logic_vector(31 downto 0);
-p_out_tst           : out   std_logic_vector(31 downto 0);
+p_in_tst             : in    std_logic_vector(31 downto 0);
+p_out_tst            : out   std_logic_vector(31 downto 0);
 
 -------------------------------
 --System
 -------------------------------
-p_in_clk            : in    std_logic;
-p_in_rst            : in    std_logic
+p_in_clk             : in    std_logic;
+p_in_rst             : in    std_logic
 );
 end component;
 
