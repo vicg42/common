01a24015127a68b963bef1961e0282b48a1f44ef
vicg <vicg42@gmail.com>
2011-06-07 16:21:00 +0000

    ML505 - добавление\удаление - файлов настройки проекта VHD
    git-svn-id: svn://10.1.7.240:3691/veresk_m@259 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ ml505/ise/ml505_vm_prj_cfg.vhd
@@ -0,0 +1,62 @@
+-------------------------------------------------------------------------
+-- Company     : Telemix
+-- Engineer    : Golovachenko Victor
+--
+-- Create Date : 27.01.2011 16:46:55
+-- Module Name : prj_cfg
+--
+-- Description : Конфигурирование проекта Veresk_M (ОТЛАДКА НА ПЛАТЕ ML505!!!!!)
+--
+-- Revision:
+-- Revision 0.01 - File Created
+--
+-------------------------------------------------------------------------
+library ieee;
+use ieee.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
+
+package prj_cfg is
+
+--//Тип используемой платы
+constant C_BOARD_USE                         : string:="ML505";
+
+--//Управление использованием модулей проекта:
+constant C_USE_TRACK                         : string:="ON";
+constant C_USE_ETH                           : string:="ON";
+constant C_USE_HDD                           : string:="OFF";
+
+constant G_DBG_ETH                           : string:="OFF";
+constant C_DBG_HDD                           : string:="OFF";
+
+constant G_DBGCS_HDD                         : string:="OFF";
+
+
+--//Конфигурирование модулей:
+--//cfg Memory Controller
+constant C_MEMCTRL_BANK_COUNT                : integer:=1; --//max 3
+
+--//cfg HDD
+constant C_HDD_COUNT                         : integer:=1;
+constant C_HDD_HOSTBUF_SIZE                  : integer:=2; --//0-(4KB),1-(8KB),2-(16KB),3-(32KB)
+constant C_HDD_RAMBUF_SIZE                   : integer:=26;--//64MB : Определяется как 2 в степени G_HDD_RAMBUF_SIZE
+constant C_HDD_GT_DBUS                       : integer:=16; --//Настройка шины данных GT (RocketIO)
+
+--//cfg PCI-Express
+constant C_PCIEXPRESS_RST_FROM_SLOT          : integer:=0;--0/1 - Использовать сброс сгенеренный в проекта/с стота PCI-Express
+constant C_PCIEXPRESS_LINK_WIDTH             : integer:=1;--При изменении кол-ва линий
+                                                          --Необходимо перегенерить ядро PCI-Express, для чего вызвать проект core_gen
+                                                          --..board_xxx\src\pci_express\core_gen\pci_express.cgp
+
+
+--//cfg VCTRL
+constant C_DSN_VCTRL_VCH_COUNT               : integer:=3;
+
+
+end prj_cfg;
+
+
+package body prj_cfg is
+
+end prj_cfg;
+
