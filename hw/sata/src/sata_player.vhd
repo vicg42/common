540868fd0949c5fb229ac3ea8dd422c9cbae9ebf
vicg <vicg42@gmail.com>
2011-06-14 14:49:12 +0000

    SATA: sata_player.vhd - УБРАЛ LOOPBACK(заворачивание обратно) ПРИНЯТОГО примитива ALIGN
    git-svn-id: svn://10.1.7.240:3691/common@284 244e6edd-17f5-4270-a3ef-e79531b98298

--- hw/sata/src/sata_player.vhd
+++ hw/sata/src/sata_player.vhd
@@ -267,7 +267,7 @@ port map
 --------------------------------------------------
 --
 --------------------------------------------------
-p_in_rxalign           => i_rxtype(C_TALIGN),
+p_in_rxalign           => '0',--i_rxtype(C_TALIGN),
 p_in_linkup            => i_oob_status(C_PSTAT_DET_ESTABLISH_ON_BIT),
 p_in_dev_detect        => i_oob_status(C_PSTAT_DET_DEV_ON_BIT),
 p_in_d10_2_send_dis    => i_d10_2_senddis,
