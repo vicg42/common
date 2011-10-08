6747326a6e927c49f861f6a7fd94c5d4cb1da358
vicg <vicg42@gmail.com>
2011-10-08 07:33:50 +0000

    ML505 -
    git-svn-id: svn://10.1.7.240:3691/veresk_m@462 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/ise/ml505_hddsimple_prj_cfg.vhd
+++ ml505/ise/ml505_hddsimple_prj_cfg.vhd
@@ -22,38 +22,16 @@ package prj_cfg is
 constant C_BOARD_USE                         : string:="ML505";
 
 --//Управление использованием модулей проекта:
-constant C_USE_TRACK                         : string:="OFF";
-constant C_USE_ETH                           : string:="OFF";
 constant C_USE_HDD                           : string:="ON";
 
-constant G_DBG_ETH                           : string:="OFF";
 constant C_DBG_HDD                           : string:="OFF";
-
-constant G_DBGCS_HDD                         : string:="ON";
-
-
---//Конфигурирование модулей:
---//cfg Memory Controller
-constant C_MEMCTRL_BANK_COUNT                : integer:=1; --//max 3
+constant C_DBGCS_HDD                         : string:="ON";
 
 --//cfg HDD
 constant C_HDD_COUNT                         : integer:=2;
 constant C_HDD_RAMBUF_SIZE                   : integer:=25;--//32MB : Определяется как 2 в степени G_HDD_RAMBUF_SIZE
 constant C_HDD_GT_DBUS                       : integer:=16;--//Настройка шины данных GT (RocketIO)
 
---//cfg PCI-Express
-constant C_PCIEXPRESS_RST_FROM_SLOT          : integer:=0;--0/1 - Использовать сброс сгенеренный в проекта/с стота PCI-Express
-constant C_PCIEXPRESS_LINK_WIDTH             : integer:=1;--При изменении кол-ва линий
-                                                          --Необходимо перегенерить ядро PCI-Express, для чего вызвать проект core_gen
-                                                          --..board_xxx\src\pci_express\core_gen\pci_express.cgp
-
-
---//cfg VCTRL
-constant C_DSN_VCTRL_VCH_COUNT               : integer:=3;
-constant C_DSN_VCTRL_SIMPLE                  : string:="ON";
-
---//cfg clkfx - DCM LocalBus
-constant C_LBUSDCM_CLKFX_M                   : integer:=3;
 
 end prj_cfg;
 
