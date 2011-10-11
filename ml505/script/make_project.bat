e3455390d6a64d2b64ab18aabb369ce7b4597a48
vicg <vicg42@gmail.com>
2011-10-11 08:17:33 +0000

    ML505 - переименовал make_ml505_projects.bat -> make_project.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@470 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ ml505/script/make_project.bat
@@ -0,0 +1,3 @@
+rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\ml505\script\mprj_ml505_hdd_test.tcl
+C:\Xilinx\ISE_DS\ISE\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\ml505\script\mprj_ml505_hdd_vm.tcl
+
