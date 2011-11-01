79a01e1f98710361944afcb5f4f01a6098731b02
vicg <vicg42@gmail.com>
2011-11-01 04:52:24 +0000

    HSCAM - script - добавил переменную среды окружения %XILINX% в файлах *.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@492 244e6edd-17f5-4270-a3ef-e79531b98298

--- hscam/script/make_project.bat
+++ hscam/script/make_project.bat
@@ -1,2 +1,3 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\hscam\script\mprj_hscam_hdd_test.tcl
+rem C:\Xilinx\ISE_DS\ISE\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\hscam\script\mprj_hscam_hdd_test.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\hscam\script\mprj_hscam_hdd_test.tcl
 
