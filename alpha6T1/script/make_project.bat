2b55ed4a6a11df1bee59e6b8bb2b4d7d5cd76c51
vicg <vicg42@gmail.com>
2011-11-01 04:52:08 +0000

    AD6T1 - script - добавил переменную среды окружения %XILINX% в файлах *.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@491 244e6edd-17f5-4270-a3ef-e79531b98298

--- alpha6T1/script/make_project.bat
+++ alpha6T1/script/make_project.bat
@@ -1,2 +1,3 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_AD6T1_pciexp_test.tcl
+rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_AD6T1_test.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_AD6T1_test.tcl
 
