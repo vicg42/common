10d4e0ec295bb737e7b1b773a1f42259afdc2510
vicg <vicg42@gmail.com>
2011-11-01 04:51:51 +0000

    AD5T1 - script - добавил переменную среды окружения %XILINX% в файлах *.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@490 244e6edd-17f5-4270-a3ef-e79531b98298

--- alpha5T1/script/make_project.bat
+++ alpha5T1/script/make_project.bat
@@ -1,2 +1,2 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_AD5T1_vereskm.tcl
-
+rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_AD5T1_vereskm.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_AD5T1_vereskm.tcl
