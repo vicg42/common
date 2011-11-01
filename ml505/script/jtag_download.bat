504b882c535beef2ca05126115880679cf6f4cfa
vicg <vicg42@gmail.com>
2011-11-01 04:51:35 +0000

    ML505 - script - добавил переменную среды окружения %XILINX% в файлах *.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@489 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/script/jtag_download.bat
+++ ml505/script/jtag_download.bat
@@ -1,1 +1,2 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\jtag_download.cmd
+rem C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\jtag_download.cmd
+%XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\jtag_download.cmd
--- ml505/script/prom_download.bat
+++ ml505/script/prom_download.bat
@@ -1,4 +1,5 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\prom_download.cmd
+rem C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\prom_download.cmd
+%XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\prom_download.cmd
 del D:\Work\Linkos\veresk_m\ml505\firmware\*.cfi
 del D:\Work\Linkos\veresk_m\ml505\firmware\*.prm
 del D:\Work\Linkos\veresk_m\ml505\firmware\*.sgi
