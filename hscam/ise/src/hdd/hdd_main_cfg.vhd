02349c12df20a5f47085805fc53f9786ed1af6ed
vicg <vicg42@gmail.com>
2012-03-16 04:45:32 +0000

    HSCAM
    git-svn-id: svn://10.1.7.240:3691/veresk_m@764 244e6edd-17f5-4270-a3ef-e79531b98298

--- hscam/ise/src/hdd/hdd_main_cfg.vhd
+++ hscam/ise/src/hdd/hdd_main_cfg.vhd
@@ -19,7 +19,7 @@ package prj_cfg is
 --//Тип используемой платы
 constant C_PCFG_BOARD                  : string:="HSCAM";
 
-constant C_PCFG_VCTRL_USE              : string:="OFF";
+constant C_PCFG_VCTRL_USE              : string:="ON";
 constant C_PCFG_VCTRL_DBGCS            : string:="OFF";
 constant C_PCFG_FRPIX                  : integer:=1280;--110;--
 constant C_PCFG_FRROW                  : integer:=1024;--8;--
@@ -33,9 +33,9 @@ constant C_PCFG_HDD_USE                : string:="ON";
 constant C_PCFG_HDD_DBG                : string:="OFF";
 constant C_PCFG_HDD_DBGCS              : string:="OFF";
 constant C_PCFG_HDD_SH_DBGCS           : string:="OFF";
-constant C_PCFG_HDD_RAID_DBGCS         : string:="ON";
+constant C_PCFG_HDD_RAID_DBGCS         : string:="OFF";
 constant C_PCFG_HDD_COUNT              : integer:=4;
-constant C_PCFG_HDD_RAMBUF_SIZE        : integer:=28;--256MB : Определяется как 2 в степени G_HDD_RAMBUF_SIZE
+constant C_PCFG_HDD_RAMBUF_SIZE        : integer:=27;--128MB : Определяется как 2 в степени G_HDD_RAMBUF_SIZE
 constant C_PCFG_HDD_GT_DBUS            : integer:=32;--Настройка шины данных GT (RocketIO)
 constant C_PCFG_HDD_FPGA_TYPE          : integer:=3; --0/1/2/3 - "V5_GTP"/"V5_GTX"/"V6_GTX"/"S6_GTPA"
 constant C_PCFG_HDD_SH_MAIN_NUM        : integer:=0; --определяем индекс GT модуля от которого будем брать частоту для тактирования sata_dcm.vhd
