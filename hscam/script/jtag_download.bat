34c4e18efb5b7ddad80437b8363051603838826c
vicg <vicg42@gmail.com>
2012-03-22 07:47:03 +0000

    HSCAM
    git-svn-id: svn://10.1.7.240:3691/veresk_m@798 244e6edd-17f5-4270-a3ef-e79531b98298

--- hscam/script/jtag_download.bat
+++
@@ -1,1 +0,0 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\hscam\script\jtag_download.cmd
--- hscam/script/jtag_download.cmd
+++
@@ -1,7 +0,0 @@
-quit
-##setMode -bs
-##setCable -p auto
-##identify
-##assignfile -p 5 -file D:\Work\Linkos\veresk_m\hscam\firmware\hdd_simple_main.bit
-##program -p 5
-##quit
