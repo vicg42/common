409719cd5b7a31d48935a0bf01a0ed2c97c4fbe9
vicg <vicg42@gmail.com>
2012-03-26 14:54:37 +0000

    script/glob_cleanup.bat
    git-svn-id: svn://10.1.7.240:3691/veresk_m@821 244e6edd-17f5-4270-a3ef-e79531b98298

--- common/script/glob_cleanup.bat
+++ common/script/glob_cleanup.bat
@@ -4,15 +4,29 @@ for /R  %%f in ( core_gen\doc core_gen\implement core_gen\simulation ) do rmdir 
 rem // »щу везде каталог core_gen и удал€ю в нем следующие файлы:
 for /R  %%f in ( core_gen\*.prj core_gen\*.xise core_gen\*.ise core_gen\*.gise core_gen\*.asy core_gen\*.vho core_gen\*.tcl core_gen\*.txt core_gen\*.pdf core_gen\*.veo core_gen\*.lso core_gen\*.xrpt ) do del "%%f"
 
-rem // »щу везде каталог pci_express и удал€ю в нем следующие каталоги:
+rem // »щу везде каталог core_gen\core_pciexp_ep_blk_plus и удал€ю в нем следующие файлы и каталоги:
 for /R  %%f in ( core_gen\core_pciexp_ep_blk_plus\doc core_gen\core_pciexp_ep_blk_plus\example_design  core_gen\core_pciexp_ep_blk_plus\simulation  core_gen\core_pciexp_ep_blk_plus\implement) do rmdir /s /q "%%f"
-
-rem // »щу везде каталог pci_express и удал€ю в нем следующие файлы:
 for /R  %%f in ( core_gen\core_pciexp_ep_blk_plus\*.txt ) do del "%%f"
 
-rem // »щу везде каталог emac и удал€ю в нем следующие файлы:
+rem // »щу везде каталог core_gen\emac и удал€ю в нем следующие файлы и каталоги:
+for /R  %%f in ( core_gen\emac_core\doc core_gen\emac_core\simulation  core_gen\emac_core\implement) do rmdir /s /q "%%f"
 for /R  %%f in ( core_gen\emac_core\*.txt ) do del "%%f"
 
+rem // »щу везде каталог core_gen\emac_core_rgmii и удал€ю в нем следующие файлы и каталоги:
+for /R  %%f in ( core_gen\emac_core_rgmii\doc core_gen\emac_core_rgmii\simulation  core_gen\emac_core_rgmii\implement) do rmdir /s /q "%%f"
+for /R  %%f in ( core_gen\emac_core_rgmii\*.txt ) do del "%%f"
+
+rem // »щу везде каталог core_gen\mem_achcount3 и удал€ю в нем следующие файлы и каталоги:
+for /R  %%f in ( core_gen\mem_achcount3\doc core_gen\mem_achcount3\simulation  core_gen\mem_achcount3\implement) do rmdir /s /q "%%f"
+for /R  %%f in ( core_gen\mem_achcount3\*.txt ) do del "%%f"
+
+rem // »щу везде каталог core_gen\mem_achcount4 и удал€ю в нем следующие файлы и каталоги:
+for /R  %%f in ( core_gen\mem_achcount4\doc core_gen\mem_achcount4\simulation  core_gen\mem_achcount4\implement) do rmdir /s /q "%%f"
+for /R  %%f in ( core_gen\mem_achcount4\*.txt ) do del "%%f"
+
+rem // »щу везде каталог core_gen\mem_ctrl_core_axi и удал€ю в нем следующие файлы и каталоги:
+for /R  %%f in ( core_gen\mem_ctrl_core_axi\docs core_gen\mem_ctrl_core_axi\simulation  core_gen\mem_ctrl_core_axi\implement) do rmdir /s /q "%%f"
+for /R  %%f in ( core_gen\mem_ctrl_core_axi\*.txt ) do del "%%f"
 
 rem // »щу везде каталог matlab и удал€ю в нем следующие файлы:
 for /R  %%f in ( matlab\*.asv ) do del "%%f"
