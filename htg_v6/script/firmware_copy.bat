3db517cd477fe87d48f11566c6fbfeca6e1b0fbd
vicg <vicg42@gmail.com>
2012-05-04 08:17:28 +0000

    HTGV6
    git-svn-id: svn://10.1.7.240:3691/veresk_m@924 244e6edd-17f5-4270-a3ef-e79531b98298

--- htg_v6/script/firmware_copy.bat
+++ htg_v6/script/firmware_copy.bat
@@ -1,3 +1,3 @@
-copy /y D:\Work\Linkos\veresk_m\alpha6T1\ise\prj\*.bit D:\Work\Linkos\veresk_m\alpha6T1\firmware
-cd D:\Work\Linkos\veresk_m\alpha6T1\firmware
+copy /y D:\Work\Linkos\veresk_m\htg_v6\ise\prj\*.bit D:\Work\Linkos\veresk_m\htg_v6\firmware
+cd D:\Work\Linkos\veresk_m\htg_v6\firmware
 dir
