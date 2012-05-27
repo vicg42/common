638972bee8ce3c3f1f516fce4b86c2e0acb2f731
vicg <vicg42@gmail.com>
2012-05-27 10:24:33 +0000

    ML505
    git-svn-id: svn://10.1.7.240:3691/veresk_m@947 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/script/prom_download.cmd
+++ ml505/script/prom_download.cmd
@@ -3,12 +3,12 @@ setSubmode -pffserial
 addPromDevice -p 1 -name xcf32p
 addDesign -version 0 -name 0
 addDeviceChain -index 0
-addDevice -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_test_main.bit
-generate -format mcs -fillvalue FF -output D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_test_main.mcs
+addDevice -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\pupok_flash_program_ml505.bit
+generate -format mcs -fillvalue FF -output D:\Work\Linkos\veresk_m\ml505\firmware\pupok_flash_program_ml505.mcs
 setMode -bs
 setCable -port auto
 identify
-assignFile -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\vereskm_test_main.mcs
+assignFile -p 1 -file D:\Work\Linkos\veresk_m\ml505\firmware\pupok_flash_program_ml505.mcs
 setAttribute -position 1 -attr readnextdevice -value "(null)"
 Program -p 1 -e -parallel
 quit
