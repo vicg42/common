8592b57f8b175b17b78ce9cd071804651f9a3064
vicg <vicg42@gmail.com>
2012-06-03 07:46:04 +0000

    HTGV6
    git-svn-id: svn://10.1.7.240:3691/veresk_m@998 244e6edd-17f5-4270-a3ef-e79531b98298

--- htg_v6/script/make_project.bat
+++ htg_v6/script/make_project.bat
@@ -1,3 +1,2 @@
-rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\htg_v6\script\mprj_HTGV6_veresk.tcl
-%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\htg_v6\script\mprj_HTGV6_veresk.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\htg_v6\script\mprj_veresk.tcl
 
