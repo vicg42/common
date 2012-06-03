b383ac51a71d83fa645eee159a0801c347158798
vicg <vicg42@gmail.com>
2012-06-03 06:42:12 +0000

    AD5T1
    git-svn-id: svn://10.1.7.240:3691/veresk_m@978 244e6edd-17f5-4270-a3ef-e79531b98298

--- alpha5T1/script/make_project.bat
+++ alpha5T1/script/make_project.bat
@@ -1,2 +1,1 @@
-rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_AD5T1_vereskm.tcl
-%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_AD5T1_vereskm.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha5T1\script\mprj_veresk.tcl
