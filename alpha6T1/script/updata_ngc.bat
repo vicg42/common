cba356f059ecdc2a2a4c17a3cfe9b430ee0c671a
vicg <vicg42@gmail.com>
2012-06-03 08:34:12 +0000

    git-svn-id: svn://10.1.7.240:3691/veresk_m@1002 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ alpha6T1/script/updata_ngc.bat
@@ -0,0 +1,6 @@
+rem »щем каталог core_gen и копируем из нее все файлы (*.ngc,*.mif ) в каталог проекта ISE(..\ise\prj)
+
+cd D:\Work\Linkos\veresk_m\alpha6T1\ise\src\core_gen\
+for /R  %%f in ( *.ngc *.mif) do xcopy "%%f" ..\..\prj /y
+
+dir
