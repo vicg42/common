a023e0b4ddaf9236666a8885b82e04567392c8af
vicg <vicg42@gmail.com>
2012-06-03 06:40:38 +0000

    AD5T1
    git-svn-id: svn://10.1.7.240:3691/veresk_m@976 244e6edd-17f5-4270-a3ef-e79531b98298

--- alpha5T1/script/xco_copy.bat
+++
@@ -1,6 +0,0 @@
-rem »щем каталог core_gen и копируем из нее все файлы (*.xco ) в каталог проекта ISE(..\ise\src\core_gen)
-
-cd D:\Work\Linkos\veresk_m\common\hw\
-for /R  %%f in ( core_gen\*.xco ) do xcopy "%%f" d:\Work\Linkos\veresk_m\alpha5T1\ise\src\core_gen /y
-
-dir
