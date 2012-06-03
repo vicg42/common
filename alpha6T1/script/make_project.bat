07b6e83de0fef5ab33c3ed424b9e6b7e770dcb4b
vicg <vicg42@gmail.com>
2012-06-03 06:36:29 +0000

    AD6T1
    git-svn-id: svn://10.1.7.240:3691/veresk_m@973 244e6edd-17f5-4270-a3ef-e79531b98298

--- alpha6T1/script/make_project.bat
+++ alpha6T1/script/make_project.bat
@@ -1,3 +1,2 @@
-rem C:\Xilinx\ISE_DS\ISE\bin\nt\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_AD6T1_test.tcl
-%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_AD6T1_veresk.tcl
+%XILINX%\bin\nt64\xtclsh D:\Work\Linkos\veresk_m\alpha6T1\script\mprj_veresk.tcl
 
