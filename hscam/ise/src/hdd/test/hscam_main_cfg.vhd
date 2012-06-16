7a93246583c7170ab335282de2a320eb04c12bec
vicg <vicg42@gmail.com>
2012-06-16 12:54:59 +0000

    HSCAM/test
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1049 244e6edd-17f5-4270-a3ef-e79531b98298

--- hscam/ise/src/hdd/test/hscam_main_cfg.vhd
+++ hscam/ise/src/hdd/test/hscam_main_cfg.vhd
@@ -17,11 +17,11 @@ use ieee.std_logic_1164.all;
 package prj_cfg is
 
 --//Версия реализации
-constant C_PCFG_HSCAM_HDD_VERSION      : integer:=16#05#; --Верисия модуля контроллера HDD для проекта HSCAM
+constant C_PCFG_HSCAM_HDD_VERSION      : integer:=16#06#; --Верисия модуля контроллера HDD для проекта HSCAM
 
 --//Тип используемой платы
 constant C_PCFG_BOARD                  : string:="HSCAM";
-constant C_PCFG_HDD_USRIF              : string:="HOST";--"FTDI";--
+constant C_PCFG_HDD_USRIF              : string:="ALL";--"HOST";--"FTDI";--
 
 --//Конфигурирование модулей:
 constant C_PCFG_VINBUF_ONE             : string:="ON";--ON/OFF - один вх видеобуфер для VCTRL и HDD/ для VCTRL и HDD отдельные вх. видео буфера
@@ -49,12 +49,38 @@ constant C_PCFG_HDD_DBG                : string:="OFF";
 constant C_PCFG_HDD_DBGCS              : string:="ON";
 constant C_PCFG_HDD_SH_DBGCS           : string:="OFF";
 constant C_PCFG_HDD_RAID_DBGCS         : string:="ON";
-constant C_PCFG_HDD_COUNT              : integer:=4;
+constant C_PCFG_HDD_COUNT              : integer:=2;
 constant C_PCFG_HDD_RAMBUF_SIZE        : integer:=27;--128MB : Определяется как 2 в степени G_HDD_RAMBUF_SIZE
 constant C_PCFG_HDD_GT_DBUS            : integer:=32;--Настройка шины данных GT (RocketIO)
 constant C_PCFG_HDD_FPGA_TYPE          : integer:=3; --0/1/2/3 - "V5_GTP"/"V5_GTX"/"V6_GTX"/"S6_GTPA"
 constant C_PCFG_HDD_SH_MAIN_NUM        : integer:=0; --определяем индекс GT модуля от которого будем брать частоту для тактирования sata_dcm.vhd
 constant C_PCFG_HDD_SATA_GEN_DEFAULT   : integer:=0; --0/1 - SATAI/II
-constant C_PCFG_HDD_RAID_DWIDTH        : integer:=128;
+constant C_PCFG_HDD_RAID_DWIDTH        : integer:=64;
+
+constant C_PCFG_HDD_SKIP_VH            : std_logic:='1';--разрешение работы через строку при 480fps
+
+
+--//Bitmap порта p_in_cam_ctrl
+constant C_CAM_CTRL_MODE_FPS_L_BIT     : integer:=0;
+constant C_CAM_CTRL_MODE_FPS_M_BIT     : integer:=1;
+constant C_CAM_CTRL_HDD_LEDOFF_BIT     : integer:=11;
+constant C_CAM_CTRL_HDD_RST_BIT        : integer:=12;
+constant C_CAM_CTRL_HDD_MODE_L_BIT     : integer:=13;
+constant C_CAM_CTRL_HDD_MODE_M_BIT     : integer:=15;
+
+--Режимы входного потока данных для HDD
+constant C_CAM_CTRL_60FPS              : integer:=0;
+constant C_CAM_CTRL_120FPS             : integer:=1;
+constant C_CAM_CTRL_240FPS             : integer:=2;
+constant C_CAM_CTRL_480FPS             : integer:=3;
+
+--Режимы работы модуля HDD
+constant C_CAM_CTRL_HDD_WR             : integer:=1;
+constant C_CAM_CTRL_HDD_RD             : integer:=2;
+constant C_CAM_CTRL_HDD_STOP           : integer:=3;
+constant C_CAM_CTRL_HDD_TEST           : integer:=4;
+constant C_CAM_CTRL_HDD_CLR_ERR        : integer:=5;
+constant C_CAM_CTRL_VCH_ON             : integer:=6;
+constant C_CAM_CTRL_CFGFTDI            : integer:=7;
 
 end prj_cfg;
