57b9f238c3e70234febe79045cf288bae45abb51
vicg <vicg42@gmail.com>
2012-09-12 15:02:29 +0000

    HTG_V6
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1110 244e6edd-17f5-4270-a3ef-e79531b98298

--- htg_v6/script/prom_download.bat
+++ htg_v6/script/prom_download.bat
@@ -1,1 +1,1 @@
-C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\htg_v6\script\prom_download.cmd
+%XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\htg_v6\script\prom_download.cmd
