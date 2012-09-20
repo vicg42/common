ab567b8d621c2fcefafd94936ad4768340def9be
vicg <vicg42@gmail.com>
2012-09-20 13:11:06 +0000

    ML505 -
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1122 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/script/jtag_download.bat
+++ ml505/script/jtag_download.bat
@@ -1,2 +1,1 @@
-rem C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\jtag_download.cmd
 %XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\jtag_download.cmd
