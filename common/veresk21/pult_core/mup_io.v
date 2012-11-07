6a8b6f4809ba4a774f933a0e622fe469e61dd159
vicg <vicg42@gmail.com>
2012-11-07 05:44:30 +0000

    veresk21/ + lata src
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1188 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ common/veresk21/pult_core/mup_io.v
@@ -0,0 +1,269 @@
+//---------------------------------------------------------
+//  Модуль обмена с Модулем Универсальным Пульта (МУП).
+// На МУП передается адресный байт и 2 байта управления
+//  светодиодами, от МУП приходит 2 байта состояния кнопок,
+//  3 байта с АЦП. Если МУП ответил, то выставляется
+//  сигнал 'answer', а если произошла ошибка четности, то
+//  выставляется признак 'error'.
+//---------------------------------------------------------
+// Автор Латушко А.В.
+//
+// V1.0   6.7.5
+//---------------------------------------------------------
+module mup_io(rst,clk,data_i,data_o,dir_485,
+              start,busy,error,answer,
+              n_mup,led,but,an_data
+              );
+
+   input rst;             //сброс FSM
+   input clk;             //тактовая (4х битовой частоты обмена)
+   input data_i;          //данные из 485 приемника
+   output data_o;         //данные на 485 передатчик
+   output dir_485;        //направление работы 485 ПП
+   input start;           //начало цикла обмена
+   output busy;           //готовность модуля обменяться с МУП
+   output error;          //ошибка обмена с МУП
+   output answer;         //МУП отвечает
+   input [2:0] n_mup;     //номер МУПа для обращения
+   input [15:0] led;      //слово управления светодиодами
+   output [15:0] but;     //слово состояния кнопок
+   output [23:0] an_data; //данные из АЦП
+
+// Внутренние переменные
+   reg data_rec,data_rec1;
+   reg [3:0] state;      //состояние FSM
+   reg [3:0] state_ret;  //адрес возврата из состояния ожидания нового байта от МУП
+   parameter S_W=0;
+   parameter S_S_NMUP=1;
+   parameter S_S_1LED=2;
+   parameter S_S_2LED=3;
+   parameter S_W_ANS=4;
+   parameter S_R_1BUT=5;
+   parameter S_R_2BUT=6;
+   parameter S_R_1AN=7;
+   parameter S_R_2AN=8;
+   parameter S_R_3AN=9;
+   parameter S_TO=10;
+   reg data_o;
+   reg busy,error,answer;
+   parameter insid=0;    //направление работы 485 приемопередатчика
+   parameter outside=1;
+   reg dir_485;           //управление направлением обмена по 485
+   reg [5:0] count;       //счетчик тактов внутри состояний 'state'
+   reg [15:0] but;
+   reg [7:0] t_rec;  //временное хранилище принятого байта (до проверки на ошибку)
+   reg [23:0] an_data;
+
+// Входной триггер и задержка на такт для определения начала старт-бита
+always @(posedge clk)
+   begin
+      data_rec <= data_i;
+      data_rec1 <= data_rec;
+   end
+
+// FSM для обмена с МУП
+always @(posedge rst or posedge clk)
+   if(rst) begin
+         state <= S_W;
+         count <= 0;
+         dir_485 <= insid;
+         data_o <= 1;
+         busy <= 0;
+         error <= 0;
+         answer <= 0;
+      end
+   else case(state)
+      S_W: begin
+            if(start) state <= S_S_NMUP;    //ждем начала обмена
+            count <= 0;
+            dir_485 <= outside;
+            busy <= 0;
+            data_o <= 1;
+         end
+// пересылка адресного байта
+      S_S_NMUP: begin
+         if(count==43) count <= 0; else count <= count+1;
+         case(count)
+            0,1,2,3: begin     //старт-бит -> на выход
+              data_o <= 0;
+              busy <= 1;       //мы заняты
+              error <= 0;      //сбросили ошибку
+              answer <= 0;     //сбросили признак ответа респондента
+             end
+// перешлем [7:3] биты адресного байта нулями
+//          4,5,6,7,        // 7 бит -> на выход
+//          8,9,10,11,      // 6 бит -> на выход
+//          12,13,14,15,    // 5 бит -> на выход
+//          16,17,18,19,    // 4 бит -> на выход
+//          20,21,22,23:    // 3 бит -> на выход
+            24,25,26,27: data_o <= n_mup[2];  // 2 бит номера МУПа -> на выход
+            28,29,30,31: data_o <= n_mup[1];  // 1 бит номера МУПа -> на выход
+            32,33,34,35: data_o <= n_mup[0];  // 0 бит номера МУПа -> на выход
+            36,37,38,39: data_o <= ^n_mup;    //поехала четность адресного байта
+            40,41,42:    data_o <= 1;         // стоп бит -> на выход
+            43: state <= S_S_1LED; //будем передавать первый байт со светодиодами
+         endcase
+      end      // конец пересылки адресного байта
+// пересылаем первый байт со светодиодами
+      S_S_1LED: begin
+         if(count==43) count <= 0; else count <= count+1;
+         case(count)
+            0,1,2,3:     data_o <= 0;          //старт-бит -> на выход
+            4,5,6,7:     data_o <= led[15];    //светодиоды -> на выход
+            8,9,10,11:   data_o <= led[14];
+            12,13,14,15: data_o <= led[13];
+            16,17,18,19: data_o <= led[12];
+            20,21,22,23: data_o <= led[11];
+            24,25,26,27: data_o <= led[10];
+            28,29,30,31: data_o <= led[9];
+            32,33,34,35: data_o <= led[8];
+            36,37,38,39: data_o <= ^led[15:8]; //поехала четность
+            40,41,42:    data_o <= 1;          // стоп бит -> на выход
+            43: state <= S_S_2LED; //будем передавать 2-й байт со светодиодами
+         endcase
+      end
+// пересылаем 2-й байт со светодиодами
+      S_S_2LED: begin
+         if(count==43) count <= 0; else count <= count+1;
+         case(count)
+            0,1,2,3:     data_o <= 0;         //старт-бит -> на выход
+            4,5,6,7:     data_o <= led[7];    //светодиоды -> на выход
+            8,9,10,11:   data_o <= led[6];
+            12,13,14,15: data_o <= led[5];
+            16,17,18,19: data_o <= led[4];
+            20,21,22,23: data_o <= led[3];
+            24,25,26,27: data_o <= led[2];
+            28,29,30,31: data_o <= led[1];
+            32,33,34,35: data_o <= led[0];
+            36,37,38,39: data_o <= ^led[7:0]; //поехала четность
+            40,41,42:    data_o <= 1;         // стоп бит -> на выход
+            43: begin
+                state <= S_W_ANS;        //идем ждать ответа от МУП
+                state_ret <= S_R_1BUT;
+                dir_485 <= insid;
+              end
+         endcase
+      end
+// Принимаем 1-й байт кнопок (сюда мы попали в середине старт-бита)
+       S_R_1BUT: begin
+         if(count==36) count <= 0; else count <= count+1;
+          case(count)
+//           0,1,2,3:    //пропускаем старт-бит
+             4:  t_rec[7] <= data_rec;
+             8:  t_rec[6] <= data_rec;
+             12: t_rec[5] <= data_rec;
+             16: t_rec[4] <= data_rec;
+             20: t_rec[3] <= data_rec;
+             24: t_rec[2] <= data_rec;
+             28: t_rec[1] <= data_rec;
+             32: t_rec[0] <= data_rec;
+             36: begin
+                   state <= S_W_ANS;      //идем ждать следующий байт от МУП
+                   state_ret <= S_R_2BUT;
+                   if(data_rec==^t_rec) but[15:8] <= t_rec[7:0];  //паритет OK
+                   else error <= 1;
+                   answer <= 1;          //респондент ответил
+                end
+          endcase
+       end
+// Принимаем 2-й байт кнопок (сюда мы попали в середине старт-бита)
+       S_R_2BUT: begin
+         if(count==36) count <= 0; else count <= count+1;
+          case(count)
+//           0,1,2,3:    //пропускаем старт-бит
+             4:  t_rec[7] <= data_rec;
+             8:  t_rec[6] <= data_rec;
+             12: t_rec[5] <= data_rec;
+             16: t_rec[4] <= data_rec;
+             20: t_rec[3] <= data_rec;
+             24: t_rec[2] <= data_rec;
+             28: t_rec[1] <= data_rec;
+             32: t_rec[0] <= data_rec;
+             36: begin
+                   state <= S_W_ANS;      //идем ждать следующий байт от МУП
+                   state_ret <= S_R_1AN;
+                   if(data_rec==^t_rec) but[7:0] <= t_rec[7:0];  //паритет OK
+                   else error <= 1;
+                end
+          endcase
+       end
+// Принимаем 1-й байт АЦП (сюда мы попали в середине старт-бита)
+       S_R_1AN: begin
+         if(count==36) count <= 0; else count <= count+1;
+          case(count)
+//           0,1,2,3:    //пропускаем старт-бит
+             4:  t_rec[7] <= data_rec;
+             8:  t_rec[6] <= data_rec;
+             12: t_rec[5] <= data_rec;
+             16: t_rec[4] <= data_rec;
+             20: t_rec[3] <= data_rec;
+             24: t_rec[2] <= data_rec;
+             28: t_rec[1] <= data_rec;
+             32: t_rec[0] <= data_rec;
+             36: begin
+                   state <= S_W_ANS;      //идем ждать следующий байт от МУП
+                   state_ret <= S_R_2AN;
+                   if(data_rec==^t_rec) an_data[23:16] <= t_rec[7:0];  //паритет OK
+                   else error <= 1;
+                end
+          endcase
+       end
+// Принимаем 2-й байт АЦП (сюда мы попали в середине старт-бита)
+       S_R_2AN: begin
+         if(count==36) count <= 0; else count <= count+1;
+          case(count)
+//           0,1,2,3:    //пропускаем старт-бит
+             4:  t_rec[7] <= data_rec;
+             8:  t_rec[6] <= data_rec;
+             12: t_rec[5] <= data_rec;
+             16: t_rec[4] <= data_rec;
+             20: t_rec[3] <= data_rec;
+             24: t_rec[2] <= data_rec;
+             28: t_rec[1] <= data_rec;
+             32: t_rec[0] <= data_rec;
+             36: begin
+                   state <= S_W_ANS;      //идем ждать следующий байт от МУП
+                   state_ret <= S_R_3AN;
+                   if(data_rec==^t_rec) an_data[15:8] <= t_rec[7:0];  //паритет OK
+                   else error <= 1;
+                end
+          endcase
+       end
+// Принимаем 3-й байт АЦП (сюда мы попали в середине старт-бита)
+       S_R_3AN: begin
+         if(count==36) count <= 0; else count <= count+1;
+          case(count)
+//           0,1,2,3:    //пропускаем старт-бит
+             4:  t_rec[7] <= data_rec;
+             8:  t_rec[6] <= data_rec;
+             12: t_rec[5] <= data_rec;
+             16: t_rec[4] <= data_rec;
+             20: t_rec[3] <= data_rec;
+             24: t_rec[2] <= data_rec;
+             28: t_rec[1] <= data_rec;
+             32: t_rec[0] <= data_rec;
+             36: begin
+                   state <= S_TO;      //пауза после обмена
+                   if(data_rec==^t_rec) an_data[7:0] <= t_rec[7:0];  //паритет OK
+                   else error <= 1;
+                end
+          endcase
+       end
+// Пауза для сброса всех МУП
+       S_TO: begin
+          count <= count+1;
+          if(count==63) state <= S_W;
+       end
+// Ждем ответа от МУП в течение 64 тактов либо ждем
+//  прихода очередного байта посылки
+       S_W_ANS: begin
+          if(count==63) state <= S_W;  //мы ждали, а нам не ответили
+          else if(~data_rec && data_rec1) begin
+                    state <= state_ret; //есть обмен
+                    count <= 0;
+                 end
+          else count <= count+1;
+       end        //end of S_W_ANS
+   endcase       //end of FSM
+
+endmodule
---
+++ common/veresk21/pult_core/pult_io.v
@@ -0,0 +1,155 @@
+//-------------------------------------------------------------
+//  Модуль обмена с пультом (несколько МУПов)
+//-------------------------------------------------------------
+// Автор Латушко А.В.
+//
+// V1.0   6.7.5
+// V2.0   31.8.5
+// V3.0   22.9.6
+//-------------------------------------------------------------
+module pult_io(rst,clk_io,trans_ack,
+               data_i,data_o,dir_485,
+               host_clk_wr,wr_en,data_from_host,
+               host_clk_rd,rd_en,data_to_host,
+               busy,ready
+               );
+
+   input rst,clk_io;            //сброс и тактовая для обмена с пультом
+   input trans_ack;             //контроллер PCI совершил обмен с кем-то
+
+   input data_i;                //последовательные данные от пульта
+   output data_o;               //последовательные данные на пульт
+   output dir_485;              //управление 485 приемо-передатчиком
+
+   input host_clk_wr;           //тактовая от хоста для записи
+   input wr_en;                 //разрешение записи в память светодиодов
+   input [31:0] data_from_host; //данные из хоста для записи
+
+   input host_clk_rd;           //тактовая от хоста для чтения
+   input rd_en;                 //разрешение записи в память светодиодов
+   output [31:0] data_to_host;  //данные в хост
+
+   output busy,ready;           //состояние обмена с пультом
+
+// Внутренние переменные
+   wire wr_en,rd_en;
+   reg start_mup;             //запрос обмена с одним МУПом
+   reg [2:0] n_mup;           //номер МУПа (МУПов -> 8 шт.)
+
+   reg [2:0] state;           //состояния FSM
+   parameter S_W=0;
+   parameter S_1=1;
+   parameter S_2=2;
+   parameter S_3=3;
+   parameter S_4=4;
+
+   reg rd_en_m,wr_en_m;     //чтение данных для пульта, запись данных от пульта
+   wire [31:0] dout;        //данные из фифо на МУП
+   wire busy_mup;           //занятость модуля обмена с МУП
+   wire [15:0] but;         //шина кнопок
+   wire [23:0] an_data;     //шина аналоговых данных
+   wire error,answer;       //состояние текущего обмена с МУП
+   reg [31:0] din;          //регистр для перезаписи в фифо
+   reg rst_ififo;           //сбросим премное фифо от лишних данных
+   wire empty_i;            //пустота фифо данных для пульта
+   wire empty_o;            //пустота фифо данных от пульта
+
+   wire [31:0] data_from_host;
+   wire [31:0] data_to_host;
+
+   reg busy;                  //мы заняты обменом
+   wire ready;                //готовность данных от пульта
+
+// Данные от PCI для светодиодов
+fifo511x32th To_pult_fifo (
+    .din(data_from_host),
+    .rd_clk(clk_io),
+    .rd_en(rd_en_m),
+    .rst(rst | rst_ififo),
+    .wr_clk(host_clk_wr),
+    .wr_en(wr_en),
+    .dout(dout),
+    .empty(empty_i),
+    .full());
+
+// Данные для PCI (кнопки, АЦП и признаки об обменах с МУПами)
+fifo511x32th From_pult_fifo (
+    .din(din),
+    .rd_clk(host_clk_rd),
+    .rd_en(rd_en),
+    .rst(rst),
+    .wr_clk(clk_io),
+    .wr_en(wr_en_m),
+    .dout(data_to_host),
+    .empty(empty_o),
+    .full());
+
+assign ready = ~empty_o & ~busy;
+
+// Поочередное общение со всеми МУПами пульта
+// Первое данное из фифо не читаем (fall-through) !!
+always @(posedge rst or posedge clk_io)
+   if(rst) begin
+         state <= S_W;
+         start_mup <= 0;
+         n_mup <= 0;
+         rd_en_m <= 0;
+         wr_en_m <= 0;
+         rst_ififo <= 0;
+         busy <= 0;
+      end
+   else case(state)
+      S_W: if(trans_ack && ~empty_i) begin
+            state <= S_1;
+            start_mup <= 1;
+            n_mup <= 0;
+            busy <= 1;
+         end
+      S_1: if(busy_mup) begin
+            state <= S_2;
+            start_mup <= 0;
+         end
+      S_2: if(~busy_mup) begin      //обмен закончился
+            state <= S_3;
+            din <= {error,answer,14'h0000,but};
+            wr_en_m <= 1;
+            if(n_mup==1 || n_mup==3 || n_mup==5) rd_en_m <= 1;
+         end
+      S_3: begin
+            state <= S_4;
+            din <= {8'h00,an_data[23:0]};
+            rd_en_m <= 0;
+            if(n_mup==7) rst_ififo <= 1;   //остальные данные нам не нужны
+         end
+      S_4: if(n_mup==7) begin
+            state <= S_W;
+            wr_en_m <= 0;
+            rst_ififo <= 0;
+            busy <= 0;
+         end
+        else begin
+            state <= S_1;
+            start_mup <= 1;
+            n_mup <= n_mup+1;
+            wr_en_m <= 0;
+         end
+      endcase
+
+// Установим устройство обмена с МУПами по 485 интерфейсу
+mup_io My_mup_io (
+    .rst(rst),
+    .clk(clk_io),
+    .data_i(data_i),
+    .data_o(data_o),
+    .dir_485(dir_485),
+    .start(start_mup),
+    .busy(busy_mup),
+    .error(error),
+    .answer(answer),
+    .n_mup(n_mup),
+    .led(n_mup[0]? dout[31:16]: dout[15:0]),
+    .but(but),
+    .an_data(an_data)
+    );
+
+endmodule
