5d519033b22c1c0d3d81fd59dcc5aa3e921ae0f4
vicg <vicg42@gmail.com>
2012-11-13 13:59:21 +0000

    VERESK/firmware - 0346
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1198 244e6edd-17f5-4270-a3ef-e79531b98298

--- htg_v6/ise/veresk_prj_cfg_gmii.vhd
+++ htg_v6/ise/veresk_prj_cfg_gmii.vhd
@@ -35,7 +35,7 @@ constant C_PCFG_VCTRL_VCH_COUNT        : integer:=6; --//max 6
 constant C_PCFG_ETH_USE                : string:="ON";
 constant C_PCFG_ETH_DBG                : string:="OFF";
 constant C_PCFG_ETH_GTCH_COUNT_MAX     : integer:=1; --Кол-во каналов в одном GT(RocketIO) модуле
-constant C_PCFG_ETH_PHY_SEL            : integer:=3;
+constant C_PCFG_ETH_PHY_SEL            : integer:=3;--0/3 - FIBER/COPPER_GMII
 
 end prj_cfg;
 
