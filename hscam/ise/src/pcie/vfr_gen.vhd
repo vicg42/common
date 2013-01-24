ccc7f932da9d2c08b5d2fd5b71663e16c0789999
vicg <vicg42@gmail.com>
2013-01-24 07:12:34 +0000

    HSCAM/pcie - vfr_gen.vhd
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1239 244e6edd-17f5-4270-a3ef-e79531b98298

--- hscam/ise/src/pcie/vfr_gen.vhd
+++ hscam/ise/src/pcie/vfr_gen.vhd
@@ -55,7 +55,7 @@ S_SYN_H,
 S_SYN_V
 );
 signal fsm_cs : fsm_state;
-
+signal i_cfg                : std_logic_vector(p_in_cfg'range);
 type TVData is array (0 to G_VD_WIDTH/8) of std_logic_vector(7 downto 0);
 signal i_vd                 : TVData;
 signal i_pix_cnt            : std_logic_vector(15 downto 0);
@@ -65,6 +65,7 @@ signal i_vs                 : std_logic;
 signal i_vd_out             : std_logic_vector(G_VD_WIDTH - 1 downto 0);
 signal i_div                : std_logic;
 signal i_row_half           : std_logic;
+signal i_vrow_half_count    : std_logic_vector(i_row_cnt'range);
 signal tst_fsm_cs,tst_fsm_cs_dly: std_logic_vector(1 downto 0);
 
 
@@ -75,7 +76,8 @@ begin
 --//Технологические сигналы
 --//----------------------------------
 p_out_tst(1 downto 0) <= tst_fsm_cs_dly;
-p_out_tst(31 downto 2) <= (others=>'0');
+p_out_tst(2) <= i_row_half;
+p_out_tst(31 downto 3) <= (others=>'0');
 process(p_in_clk)
 begin
   if p_in_clk'event and p_in_clk='1' then
@@ -91,7 +93,18 @@ tst_fsm_cs <= CONV_STD_LOGIC_VECTOR(16#02#,tst_fsm_cs'length) when fsm_cs = S_SY
 --//CFG
 --//----------------------------------
 i_div <= '1';
-
+process(p_in_rst, p_in_clk)
+begin
+  if p_in_rst = '1' then
+    i_cfg <= (others=>'0');
+  elsif rising_edge(p_in_clk) then
+  if i_div = '1' then
+      if i_vs = '1' then
+      i_cfg <= p_in_cfg;
+      end if;
+  end if;
+  end if;
+end process;
 
 --//----------------------------------
 --//Video
@@ -101,6 +114,8 @@ p_out_vs <= i_vs when G_VSYN_ACTIVE = '1' else not i_vs;
 p_out_hs <= i_hs when G_VSYN_ACTIVE = '1' else not i_hs;
 p_out_vclk <= p_in_clk;
 p_out_vclk_en <= i_div;
+
+i_vrow_half_count <= '0' & p_in_vrow(p_in_vrow'length - 1 downto 1);
 
 --vsync
 process(p_in_rst, p_in_clk)
@@ -134,7 +149,7 @@ begin
               fsm_cs <= S_SYN_H;
             end if;
 
-            if i_row_cnt = ( ('0'&p_in_vrow(p_in_vrow'length - 1 downto 1)) - 1) then
+            if i_row_cnt = (i_vrow_half_count - 1) then
               i_row_half <= '1';
             end if;
 
@@ -183,27 +198,56 @@ begin
     end loop;
   elsif rising_edge(p_in_clk) then
   if i_div = '1' then
-    if i_row_half = '0' then
-      if i_hs = '1' or i_vs = '1' then
-        for i in 0 to G_VD_WIDTH/8 - 1 loop
-        i_vd(i) <= CONV_STD_LOGIC_VECTOR(i, i_vd(i)'length);
-        end loop;
-      else
-        for i in 0 to G_VD_WIDTH/8 - 1 loop
-        i_vd(i) <= i_vd(i) + CONV_STD_LOGIC_VECTOR(G_VD_WIDTH/8, i_vd(i)'length);
-        end loop;
+
+      if i_cfg(5 downto 4) = "00" then
+      --(вертикальные полоски)
+          if i_hs = '1' or i_vs = '1' then
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= CONV_STD_LOGIC_VECTOR(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= i_vd(i) + CONV_STD_LOGIC_VECTOR(G_VD_WIDTH/8, i_vd(i)'length);
+            end loop;
+          end if;
+
+      elsif i_cfg(5 downto 4) = "01" then
+      --(горизонтальные полоски)
+          if i_hs = '1' or i_vs = '1' then
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= (others=>'0');
+            end loop;
+          else
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= i_row_cnt(i_vd(i)'range);
+            end loop;
+          end if;
+
+      elsif i_cfg(5 downto 4) = "10" then
+      --(вертикальные + горизонтальные полоски)
+        if i_row_half = '0' then
+          if i_hs = '1' or i_vs = '1' then
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= CONV_STD_LOGIC_VECTOR(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= i_vd(i) + CONV_STD_LOGIC_VECTOR(G_VD_WIDTH/8, i_vd(i)'length);
+            end loop;
+          end if;
+        else
+          if i_vs = '1' then
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= CONV_STD_LOGIC_VECTOR(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to G_VD_WIDTH/8 - 1 loop
+            i_vd(i) <= i_row_cnt(i_vd(i)'range);
+            end loop;
+          end if;
+        end if;
       end if;
-    else
-      if i_vs = '1' then
-        for i in 0 to G_VD_WIDTH/8 - 1 loop
-        i_vd(i) <= CONV_STD_LOGIC_VECTOR(i, i_vd(i)'length);
-        end loop;
-      elsif i_hs = '1' then
-        for i in 0 to G_VD_WIDTH/8 - 1 loop
-        i_vd(i) <= i_row_cnt(i_vd(i)'range);
-        end loop;
-      end if;
-    end if;
+
   end if;
   end if;
 end process;
