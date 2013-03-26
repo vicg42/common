2371a54df3b99f65a1e14234dc1768d7539b99b1
vicg <vicg42@gmail.com>
2013-03-26 11:45:54 +0000

    ML505
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1301 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/script/prom_download.bat
+++ ml505/script/prom_download.bat
@@ -1,4 +1,3 @@
-rem C:\Xilinx\ISE_DS\ISE\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\prom_download.cmd
 %XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\ml505\script\prom_download.cmd
 del D:\Work\Linkos\veresk_m\ml505\firmware\*.cfi
 del D:\Work\Linkos\veresk_m\ml505\firmware\*.prm
