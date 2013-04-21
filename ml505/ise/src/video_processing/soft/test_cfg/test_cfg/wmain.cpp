a8d6cc38064af3e1263c7fc1515e182897a710b0
vicg <vicg42@gmail.com>
2013-04-21 13:59:38 +0000

    ML505/vproc (soft)
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1319 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/ise/src/video_processing/soft/test_cfg/test_cfg/wmain.cpp
+++ ml505/ise/src/video_processing/soft/test_cfg/test_cfg/wmain.cpp
@@ -96,8 +96,8 @@ MainWindow::MainWindow(QWidget *parent)
     connect(udev.eth.udpSocket, SIGNAL(readyRead()),
             this, SLOT(eth_rxd()));
 
-    connect(btn_usr_set, SIGNAL(clicked()),
-            this, SLOT(get_firmware()));
+//    connect(btn_usr_set, SIGNAL(clicked()),
+//            this, SLOT(board_init()));
 
     connect(btn_eth, SIGNAL(clicked()),
             this, SLOT(eth_on_off()));
@@ -105,7 +105,16 @@ MainWindow::MainWindow(QWidget *parent)
     connect(btn_img_open, SIGNAL(clicked()),
             this, SLOT(img_open()));
 
+    TVCH_prm  vch;
+    vch.fr_size.activ.x = 1024/4;
+    vch.fr_size.activ.y = 1024;
+    vch.fr_size.skip.x = 0;
+    vch.fr_size.skip.y = 0;
 
+    fg.vch_count = C_BOARD_VCH_COUNT_MAX;
+    for (uint8_t idx = 0; idx < fg.vch_count; idx++){
+      fg.vch[idx] = vch;
+    }
 }
 
 MainWindow::~MainWindow()
@@ -121,13 +130,9 @@ void MainWindow::eth_rxd()
         rxd.resize(udev.eth.udpSocket->pendingDatagramSize());
         udev.eth.udpSocket->readDatagram(rxd.data(), rxd.size());
 
-//        etext_log->append(QString("%1: size=0x%2")
-//                          .arg(__func__)
-//                          .arg(rxd.size(), 0 , 16));
-
         qint16 *rxd16 = (qint16 *)rxd.data();
 
-        if ((rxd16[0] & 0xF) == C_ETH_PKT_TYPE_CFG){
+        if ((rxd16[0] & 0xF) == C_PKT_TYPE_CFG){
             etext_log->append(QString("%1: firmware=0x%2")
                               .arg(__func__)
                               .arg(rxd16[3], 0 , 16));
@@ -141,7 +146,7 @@ void MainWindow::cfg_txd()
     QByteArray txd;
     txd.resize((3 + 1) * sizeof(qint16));
     qint16 *txd16 = (qint16 *)txd.data();
-    txd16[0] = (C_ETH_PKT_TYPE_CFG << C_CFGPKT_TYPE_BIT) |
+    txd16[0] = (C_PKT_TYPE_CFG << C_CFGPKT_TYPE_BIT) |
             (C_CFGPKT_WR << C_CFGPKT_WR_BIT) |
             (0 << C_CFGPKT_FIFO_BIT) |
             ((C_CFGDEV_TESTING & C_CFGPKT_DADR_MASK) << C_CFGPKT_DADR_L_BIT);
@@ -159,30 +164,36 @@ void MainWindow::cfg_txd()
                       .arg(txd16[3], 0 , 16));
 }
 
-bool MainWindow::get_firmware()
-{
-    QByteArray txd;
-    txd.resize((3 + 0) * sizeof(qint16));
-    qint16 *txd16 = (qint16 *)txd.data();
-    txd16[0] = (C_ETH_PKT_TYPE_CFG << C_CFGPKT_TYPE_BIT) |
-            (C_CFGPKT_RD << C_CFGPKT_WR_BIT) |
-            (0 << C_CFGPKT_FIFO_BIT) |
-            ((C_CFGDEV_TESTING & C_CFGPKT_DADR_MASK) << C_CFGPKT_DADR_L_BIT);
-    txd16[1] = (0 & C_CFGPKT_RADR_MASK) << C_CFGPKT_RADR_L_BIT;
-    txd16[2] = (1 & C_CFGPKT_DLEN_MASK) << C_CFGPKT_DLEN_L_BIT;
 
-    const ssize_t written = dev_write((quint8 *) txd.data(), (qint64) txd.size(), 0);
-    if (written == -1)
-        return false; //throw L::system_error();
+bool MainWindow::board_init(void) {
+  bool result;
 
-    if (static_cast<size_t>(written) != (size_t)txd.size())
-        return false; //throw runtime_error("Writing error");*/
-    else
-        return true;
+  result = get_firmware();
+  if (!result)
+    return result;
+
+  const uint8_t vch_count = 1;
+
+  for (uint8_t idx = 0; idx < vch_count; idx++){
+    result = set_vch_prm(idx, fg.vch[idx]);
+    if (!result)
+      return result;
+  }
+
+  return result;
 }
 
-void MainWindow::eth_on_off()
-{
+
+bool MainWindow::get_firmware(void) {
+  bool result;
+  result = cfg_read(C_CFGDEV_TESTING, 0,
+                    C_PKT_TYPE_CFG, NULL, 1, 0,
+                    C_BOARD_IF);
+
+  return result;
+}
+
+void MainWindow::eth_on_off() {
     bool ok;
     QString str = eline_eth_port->text();
     udev.eth.ip = QHostAddress(eline_eth_ip->text());
@@ -197,7 +208,7 @@ void MainWindow::eth_on_off()
                           .arg(udev.eth.ip.toString())
                           .arg(udev.eth.port, 0 , 10));
 
-        ok = get_firmware();
+        ok = board_init();
     }
     else {
         udev.eth.udpSocket->close();
@@ -250,79 +261,178 @@ void MainWindow::img_open()
 
 bool MainWindow::imgToboard(QImage *img)
 {
-    size_t frame = 0;
-    size_t split = 1024;
-    size_t width = img->width();
+  bool result = true;
+  size_t frame = 0;
+  size_t split = 1024;
+  size_t width = img->width();
 
-    if (width % 4)
-    {
-        width += 4 - width % 4;
-        //cout << "Warning: Image width is realigned, new width is " << width << endl;
-    }
+  if (width % 4)
+  {
+      width += 4 - width % 4;
+      //cout << "Warning: Image width is realigned, new width is " << width << endl;
+  }
 
-    if (!split)
-        split = width;
-    else
-    if (split >= width)
-    {
-        split = width;
-        //cout << "Warning: Too small image width (will not be splitted)" << endl;
-    }
+  if (!split)
+      split = width;
+  else
+  if (split >= width)
+  {
+      split = width;
+      //cout << "Warning: Too small image width (will not be splitted)" << endl;
+  }
 
-    const QTime ct = QTime::currentTime();
-    //cout << "Image timestamp: " << qPrintable(ct.toString("hh:mm:ss.zzz")) << endl;
-    const uint32_t ts = LDCU::Timestamp::Make(ct);
+  const QTime ct = QTime::currentTime();
+  //cout << "Image timestamp: " << qPrintable(ct.toString("hh:mm:ss.zzz")) << endl;
+  const uint32_t ts = LDCU::Timestamp::Make(ct);
 
-    const size_t HEAD_SIZE = 16;
-    std::vector<uint8_t> buffer;
+  const size_t HEAD_SIZE = 16;
+  std::vector<uint8_t> buffer;
 
-    for (size_t y = 0, ylim = img->height(); y < ylim; ++y)
-    {
-        //cout << L::Console::Cursor::Restore << (y * 100 / ylim) << "%" << flush;
+  for (size_t y = 0, ylim = img->height(); y < ylim; ++y)
+  {
+      //cout << L::Console::Cursor::Restore << (y * 100 / ylim) << "%" << flush;
 
-        for (size_t chunk = 0, clim = (width + split - 1) / split; chunk < clim; ++chunk)
-        {
-            size_t chunk_width = (width - chunk * split >= split) ? split : width - chunk * split;
+      for (size_t chunk = 0, clim = (width + split - 1) / split; chunk < clim; ++chunk)
+      {
+          size_t chunk_width = (width - chunk * split >= split) ? split : width - chunk * split;
 
-            buffer.resize(HEAD_SIZE + chunk_width);
+          buffer.resize(HEAD_SIZE + chunk_width);
 
-            *reinterpret_cast<uint16_t *>(&buffer[0]) = frame;
-            *reinterpret_cast<uint16_t *>(&buffer[2]) = width;
-            *reinterpret_cast<uint16_t *>(&buffer[4]) = img->height();
-            *reinterpret_cast<uint16_t *>(&buffer[6]) = y;
-            *reinterpret_cast<uint16_t *>(&buffer[8]) = chunk * split;
-            *reinterpret_cast<uint16_t *>(&buffer[10]) = ts & 0x0000ffff;
-            *reinterpret_cast<uint16_t *>(&buffer[12]) = ts >> 16;
-            *reinterpret_cast<uint16_t *>(&buffer[14]) = 0; // alignment
+          *reinterpret_cast<uint16_t *>(&buffer[0]) = frame;
+          *reinterpret_cast<uint16_t *>(&buffer[2]) = width;
+          *reinterpret_cast<uint16_t *>(&buffer[4]) = img->height();
+          *reinterpret_cast<uint16_t *>(&buffer[6]) = y;
+          *reinterpret_cast<uint16_t *>(&buffer[8]) = chunk * split;
+          *reinterpret_cast<uint16_t *>(&buffer[10]) = ts & 0x0000ffff;
+          *reinterpret_cast<uint16_t *>(&buffer[12]) = ts >> 16;
+          *reinterpret_cast<uint16_t *>(&buffer[14]) = 0; // alignment
 
-            for (size_t x = 0, xlim = chunk_width; x < xlim; ++x)
-                buffer[HEAD_SIZE + x] = qGray(img->pixel(x + chunk * split, y));
+          for (size_t x = 0, xlim = chunk_width; x < xlim; ++x)
+              buffer[HEAD_SIZE + x] = qGray(img->pixel(x + chunk * split, y));
 
-            //const ssize_t written = write(fd, &buffer[0], buffer.size());
-            const ssize_t written = dev_write(&buffer[0], (qint64) buffer.size(), 0);
+          result = dev_write(&buffer[0], (qint64) buffer.size(), C_BOARD_IF);
+          if (!result)
+            return result;
+      }
+  }
 
-            if (written == -1)
-                return false; //throw L::system_error();
-
-            if (static_cast<size_t>(written) != buffer.size())
-                return false; //throw runtime_error("Writing error");*/
-        }
-    }
-
-    etext_log->append(QString("%1: image:%2 x %3")
-                      .arg(__func__)
-                      .arg(img->width(), 0 , 10)
-                      .arg(img->height(), 0 , 10));
-    return true;
+  return true;
 }
 
-qint64 MainWindow::dev_write(unsigned char *data, long long int dlen, unsigned char interface)
-{
-    qint64 write;
+bool MainWindow::dev_write(uint8_t *data, uint64_t dlen, uint8_t interface) {
+  uint64_t write;
 
-    if (interface == 0) {
-        write = udev.eth.udpSocket->writeDatagram((char *)data, (qint64) dlen, udev.eth.ip, udev.eth.port);
-    }
+  if (interface == 0) {
+    write = udev.eth.udpSocket->writeDatagram((char *)data, dlen, udev.eth.ip, udev.eth.port);
+    if (write != dlen)
+      return false;
+  }
 
-    return write;
+  return true;
 }
+
+bool MainWindow::set_vch_prm(uint8_t vch, TVCH_prm val) {
+  uint32_t txd;
+  bool result = true;
+
+  //Set Active Zone X,Y
+  txd = 0;
+  txd = ((val.fr_size.activ.x & 0xFFFF) << 0)
+        | ((val.fr_size.activ.y & 0xFFFF) << 16);
+  result = cfg_write(C_CFGDEV_FG, C_FR_REG_DATA_L,
+                     C_PKT_TYPE_CFG, (uint8_t *) &txd, (uint16_t) sizeof(uint32_t), 0,
+                     C_BOARD_IF);
+  if (!result) {
+    return false;
+  }
+
+  txd = 0;
+  txd = ((vch & C_FG_REG_CTRL_VCH_MASK) << C_FG_REG_CTRL_VCH_L_BIT)
+        | ((C_FG_PRM_FR_ZONE_ACTIVE & C_FG_REG_CTRL_PRM_MASK) << C_FG_REG_CTRL_PRM_L_BIT)
+        | (1 << C_FG_REG_CTRL_SET_BIT);
+  result = cfg_write(C_CFGDEV_FG, C_FR_REG_CTRL,
+                     C_PKT_TYPE_CFG, (uint8_t *) txd, (uint16_t) sizeof(uint16_t), 0,
+                     C_BOARD_IF);
+  if (!result) {
+    return false;
+  }
+
+  //Set Skip Zone X,Y
+  txd = 0;
+  txd = ((val.fr_size.skip.x & 0xFFFF) << 0)
+        | ((val.fr_size.skip.y & 0xFFFF) << 16);
+  result = cfg_write(C_CFGDEV_FG, C_FR_REG_DATA_L,
+                     C_PKT_TYPE_CFG, (uint8_t *) &txd, (uint16_t) sizeof(uint32_t), 0,
+                     C_BOARD_IF);
+  if (!result) {
+    return false;
+  }
+
+  txd = 0;
+  txd = ((vch & C_FG_REG_CTRL_VCH_MASK) << C_FG_REG_CTRL_VCH_L_BIT)
+        | ((C_FG_PRM_FR_ZONE_SKIP & C_FG_REG_CTRL_PRM_MASK) << C_FG_REG_CTRL_PRM_L_BIT)
+        | (1 << C_FG_REG_CTRL_SET_BIT);
+  result = cfg_write(C_CFGDEV_FG, C_FR_REG_CTRL,
+                     C_PKT_TYPE_CFG, (uint8_t *) txd, (uint16_t) sizeof(uint16_t), 0,
+                     C_BOARD_IF);
+  if (!result) {
+    return false;
+  }
+
+  return result;
+}
+
+
+bool MainWindow::cfg_write(uint16_t cfgdev, uint16_t sreg,
+                           uint8_t tpkt, uint8_t *data, uint16_t dlen, uint8_t fifo,
+                           uint8_t interface) {
+  bool result = true;
+  const uint16_t CI_BUFFER_SIZE = ((3 * sizeof(uint16_t)) + (dlen / sizeof(uint16_t)));
+
+  uint8_t *buf;
+
+  if((buf = (uint8_t*) malloc(CI_BUFFER_SIZE)) == NULL) {
+    return false;
+  }
+
+  *(uint16_t *)(&buf[0])= ((tpkt & C_CFGPKT_TYPE_MASK) << C_CFGPKT_TYPE_BIT)
+                          | (C_CFGPKT_WR << C_CFGPKT_WR_BIT)
+                          | (fifo << C_CFGPKT_FIFO_BIT)
+                          | ((cfgdev & C_CFGPKT_DADR_MASK) << C_CFGPKT_DADR_L_BIT);
+  *(uint16_t *)(&buf[2]) = (sreg & C_CFGPKT_RADR_MASK) << C_CFGPKT_RADR_L_BIT;
+  *(uint16_t *)(&buf[4]) = (dlen & C_CFGPKT_DLEN_MASK) << C_CFGPKT_DLEN_L_BIT;
+
+  memcpy(&buf[6], data, dlen);
+
+  result = dev_write(&buf[0], (uint64_t) CI_BUFFER_SIZE, interface);
+
+  free(buf);
+  return result;
+}
+
+bool MainWindow::cfg_read(uint16_t cfgdev, uint16_t sreg,
+                          uint8_t tpkt, uint8_t *data, uint16_t dlen, uint8_t fifo,
+                          uint8_t interface) {
+  bool result = true;
+  const uint16_t CI_BUFFER_SIZE = ((3 * sizeof(uint16_t)));
+
+  uint8_t *buf;
+
+  if((buf = (uint8_t*) malloc(CI_BUFFER_SIZE)) == NULL) {
+    return false;
+  }
+
+  *(uint16_t *)(&buf[0])= ((tpkt & C_CFGPKT_TYPE_MASK) << C_CFGPKT_TYPE_BIT)
+                          | (C_CFGPKT_RD << C_CFGPKT_WR_BIT)
+                          | (fifo << C_CFGPKT_FIFO_BIT)
+                          | ((cfgdev & C_CFGPKT_DADR_MASK) << C_CFGPKT_DADR_L_BIT);
+  *(uint16_t *)(&buf[2]) = (sreg & C_CFGPKT_RADR_MASK) << C_CFGPKT_RADR_L_BIT;
+  *(uint16_t *)(&buf[4]) = (dlen & C_CFGPKT_DLEN_MASK) << C_CFGPKT_DLEN_L_BIT;
+
+  result = dev_write(&buf[0], (uint64_t) CI_BUFFER_SIZE, interface);
+
+  free(buf);
+
+  return result;
+}
+
--- ml505/ise/src/video_processing/soft/test_cfg/test_cfg/wmain.h
+++ ml505/ise/src/video_processing/soft/test_cfg/test_cfg/wmain.h
@@ -6,13 +6,17 @@
 #include <QtGui>
 
 
-#define C_ETH_PKT_TYPE_CFG         0xA
+#define C_BOARD_IF                 0 //Интерфейс обмена с платой
+#define C_BOARD_VCH_COUNT_MAX      4
+
+#define C_PKT_TYPE_CFG             0xA
 
 //C_CFGPKT_WR_BIT/ Bit Map:
 #define C_CFGPKT_WR                0
 #define C_CFGPKT_RD                1
 //HEADER(0)/ Bit map:
 #define C_CFGPKT_TYPE_BIT          0 //Тип пакета
+#define C_CFGPKT_TYPE_MASK         0xF
 #define C_CFGPKT_FIFO_BIT          6 //Тип адресации 1 - FIFO/0 - Регистр(авто инкрементация адреса)
 #define C_CFGPKT_WR_BIT            7 //Тип пакета - запись/чтение
 #define C_CFGPKT_DADR_L_BIT        8 //Адрес модуля в проекте FPGA
@@ -26,19 +30,57 @@
 
 #define C_CFGDEV_SWT               0
 #define C_CFGDEV_ETH               1
-#define C_CFGDEV_VCTRL             2
+#define C_CFGDEV_FG                2
 #define C_CFGDEV_TMR               3
 #define C_CFGDEV_HDD               5
 #define C_CFGDEV_TESTING           6
 
 
-struct TUDev{
-    struct TEth{
-        QUdpSocket *udpSocket;
-        QHostAddress ip;
-        qint16 port;
-    }eth;
+#define C_FR_REG_CTRL                     0
+#define C_FR_REG_DATA_L                   1
+#define C_FR_REG_DATA_M                   2
+#define C_FR_REG_MEM_CTRL                 3 //(15..8)(7..0) - trn_mem_rd;trn_mem_wr
+#define C_FR_REG_TST0                     4
+
+#define C_FG_REG_CTRL_VCH_L_BIT           0 //Номер видео канала
+#define C_FG_REG_CTRL_VCH_MASK            0xF
+#define C_FG_REG_CTRL_PRM_L_BIT           4 //Номер парамера
+#define C_FG_REG_CTRL_PRM_MASK            0x7
+#define C_FG_REG_CTRL_SET_BIT             7
+#define C_FG_REG_CTRL_SET_IDLE_BIT        8
+#define C_FG_REG_CTRL_RAMCOE_ADR_BIT      9
+#define C_FG_REG_CTRL_RAMCOE_DATA_BIT     10
+#define C_FG_REG_CTRL_RAMCOE_L_BIT        11 //Номер RAMCOE
+#define C_FG_REG_CTRL_RAMCOE_MASK         0x7
+
+#define C_FG_PRM_MEM_ADR_WR               0 //Базовый адрес буфера записи видео
+#define C_FG_PRM_MEM_ADR_RD               1 //Базовый адрес буфера чтения видео
+#define C_FG_PRM_FR_ZONE_SKIP             2
+#define C_FG_PRM_FR_ZONE_ACTIVE           3
+#define C_FG_PRM_FR_OPTIONS               4
+
+struct TUDev {
+  struct TEth {
+    QUdpSocket *udpSocket;
+    QHostAddress ip;
+    qint16 port;
+  }eth;
 };
+
+struct TVCH_prm {
+  struct TVFr_size {
+    struct TXY {
+      uint16_t x;
+      uint16_t y;
+    }activ, skip;
+  }fr_size;
+};
+
+struct TFG_prm {
+  TVCH_prm  vch[C_BOARD_VCH_COUNT_MAX];
+  uint8_t  vch_count;
+};
+
 
 class MainWindow : public QWidget
 {
@@ -50,7 +92,8 @@ public:
 
 private:
 
-    TUDev udev;
+    TUDev  udev;
+    TFG_prm  fg;
 
     QLineEdit *eline_eth_ip;
     QLineEdit *eline_eth_port;
@@ -67,8 +110,19 @@ private:
 
     bool imgToboard(QImage *img);
 
-//    void dev_pkt(unsigned char *data, unsigned char *dir, unsigned char dev);
-    qint64 dev_write(unsigned char *data, long long dlen, unsigned char interface);
+    bool board_init(void);
+    bool get_firmware(void);
+    bool set_vch_prm(uint8_t vch, TVCH_prm  val);
+    bool cfg_write(uint16_t cfgdev, uint16_t sreg,
+                   uint8_t tpkt, uint8_t *data, uint16_t dlen, uint8_t fifo,
+                   uint8_t interface);
+
+    bool cfg_read(uint16_t cfgdev, uint16_t sreg,
+                   uint8_t tpkt, uint8_t *data, uint16_t dlen, uint8_t fifo,
+                   uint8_t interface);
+
+    bool dev_write(uint8_t *data, uint64_t dlen, uint8_t interface);
+
 
 private slots:
 
@@ -76,7 +130,6 @@ private slots:
     void eth_rxd();
     void eth_on_off();
     void img_open();
-    bool get_firmware();
 
 };
 
