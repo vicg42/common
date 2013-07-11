8782d977c102cb81dc3601ad9ceabaeb411d0a90
vicg <vicg42@gmail.com>
2013-07-11 13:03:59 +0000

    DINI_K7/ -rename
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1417 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ dini_k7/script/firmware_copy.bat
@@ -0,0 +1,3 @@
+copy /y D:\Work\Linkos\veresk_m\dinig_k7\ise\prj\*.bit D:\Work\Linkos\veresk_m\dinig_k7\firmware
+cd D:\Work\Linkos\veresk_m\dinig_k7\firmware
+dir
---
+++ dini_k7/script/prom_download.bat
@@ -0,0 +1,1 @@
+%XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\dinig_k7\script\prom_download.cmd
