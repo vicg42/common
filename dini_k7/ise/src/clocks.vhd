8a0b71db6a9d89bfba6749f3d47fda29daf2faf5
vicg <vicg42@gmail.com>
2013-07-18 15:01:38 +0000

    DINI_K7/ -up
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1439 244e6edd-17f5-4270-a3ef-e79531b98298

--- dini_k7/ise/src/clocks.vhd
+++ dini_k7/ise/src/clocks.vhd
@@ -37,6 +37,7 @@ end;
 
 architecture synth of clocks is
 
+signal i_pll_clkin   : std_logic;
 signal g_pll_clkin   : std_logic;
 signal i_pll_rst_cnt : std_logic_vector(4 downto 0) := "11111";
 signal i_pll_rst     : std_logic := '1';
@@ -66,18 +67,18 @@ end process;
 -- Reference clock MMCM (CLKFBOUT range 600.00 MHz to 1440.00 MHz)
 -- CLKFBOUT = (CLKIN1/DIVCLK_DIVIDE) * CLKFBOUT_MULT_F
 -- CLKOUTn  = (CLKIN1/DIVCLK_DIVIDE) * CLKFBOUT_MULT_F/CLKOUTn_DIVIDE
--- CLKFBOUT = (200 MHz/4) * 16.000       = 800 MHz
--- CLKOUT0  = (200 MHz/4) * 16.000/6.400 = 125 MHz
--- CLKOUT1  = (200 MHz/4) * 16.000/4     = 200 MHz
--- CLKOUT2  = (200 MHz/4) * 16.000/2     = 400 MHz
--- CLKOUT3  = (200 MHz/4) * 16.000/8     = 100 MHz
--- CLKOUT4  = (200 MHz/4) * 16.000/25    = 32 MHz
+-- CLKFBOUT = (400 MHz/8) * 16.000       = 800 MHz
+-- CLKOUT0  = (400 MHz/8) * 16.000/6.400 = 125 MHz
+-- CLKOUT1  = (400 MHz/8) * 16.000/4     = 200 MHz
+-- CLKOUT2  = (400 MHz/8) * 16.000/2     = 400 MHz
+-- CLKOUT3  = (400 MHz/8) * 16.000/8     = 100 MHz
+-- CLKOUT4  = (400 MHz/8) * 16.000/25    = 32 MHz
 
 mmcm_ref_clk_i : MMCME2_BASE
 generic map(
 BANDWIDTH          => "OPTIMIZED", -- string := "OPTIMIZED"
-CLKIN1_PERIOD      => 5.000,       -- real := 0.0
-DIVCLK_DIVIDE      => 4,           -- integer := 1 (1 to 128)
+CLKIN1_PERIOD      => 2.500,       -- real := 0.0
+DIVCLK_DIVIDE      => 8,           -- integer := 1 (1 to 128)
 CLKFBOUT_MULT_F    => 16.000,      -- real := 1.0  (5.0 to 64.0)
 CLKOUT0_DIVIDE_F   => 6.400,       -- real := 1.0  (1.0 to 128.0)
 CLKOUT1_DIVIDE     => 4,           -- integer := 1
@@ -102,13 +103,12 @@ CLKOUT4_DUTY_CYCLE => 0.500,       -- real := 0.5
 CLKOUT5_DUTY_CYCLE => 0.500,       -- real := 0.5
 CLKOUT6_DUTY_CYCLE => 0.500,       -- real := 0.5
 CLKOUT4_CASCADE    => FALSE,       -- boolean := FALSE
-CLOCK_HOLD         => FALSE,       -- boolean := FALSE
 REF_JITTER1        => 0.0,         -- real := 0.0
 STARTUP_WAIT       => FALSE)       -- boolean := FALSE
 port map(
 RST       => i_pll_rst,    -- in std_ulogic;
 PWRDWN    => '0',          -- in std_ulogic;
-CLKIN1    => p_in_clk.clk,  -- in std_ulogic;
+CLKIN1    => g_pll_clkin,  -- in std_ulogic;
 CLKFBIN   => g_clk_fb(0),  -- in std_ulogic;
 CLKFBOUT  => i_clk_fb(0),  -- out std_ulogic;
 CLKFBOUTB => open,         -- out std_ulogic;
@@ -138,7 +138,7 @@ bufg_clk2: BUFG port map(I => i_clk_out(3), O => p_out_gclk(2)); --100MHz
                                                  p_out_gclk(4)<='0'; --125MHz
 bufg_clk5: BUFG port map(I => i_clk_out(5), O => p_out_gclk(5));--32MHz
 --                                               p_out_gclk(6));--зарезервировано!!!
-p_out_gclk(7) <= p_in_clk.clk;--100MHz
+p_out_gclk(7) <= '0';--100MHz
 
 m_buf_pciexp : IBUFDS_GTXE1 port map (
 I     => p_in_clk.pciexp_clk_p,
