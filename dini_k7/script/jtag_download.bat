dd6e82356ad7f0abd54d0b5170f13468e04e3b34
vicg <vicg42@gmail.com>
2013-07-20 07:46:01 +0000

    DINI_K7/ -add (jtag download script)
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1445 244e6edd-17f5-4270-a3ef-e79531b98298

---
+++ dini_k7/script/jtag_download.bat
@@ -0,0 +1,1 @@
+%XILINX%\bin\nt64\impact -batch D:\Work\Linkos\veresk_m\dini_k7\script\jtag_download.cmd
---
+++ dini_k7/script/jtag_download.cmd
@@ -0,0 +1,7 @@
+setMode -bs
+setCable -p auto
+identify
+assignFile -p 3 -file "d:\Work\Linkos\veresk_m\dini_k7\script\bsdl\25632kv18_x18_165.bsd"
+assignfile -p 1 -file D:\Work\Linkos\veresk_m\dini_k7\firmware\test_led_main.bit
+program -p 1
+quit
