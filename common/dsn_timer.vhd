906aaff5890230bd395613efeef15cff4b28e57f
vicg <vicg42@gmail.com>
2013-10-02 06:13:29 +0000

    dsn_timer - правка использования библиотек ieee + cleanup + Переход к синхронному RESET
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1506 244e6edd-17f5-4270-a3ef-e79531b98298

--- common/dsn_timer.vhd
+++ common/dsn_timer.vhd
@@ -17,9 +17,6 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
-library unisim;
-use unisim.vcomponents.all;
-
 library work;
 use work.vicg_common_pkg.all;
 use work.prj_def.all;
@@ -85,11 +82,12 @@ begin
 --Конфигурирование модуля
 ----------------------------------------------------
 --Счетчик адреса регистров
-process(p_in_rst, p_in_host_clk)
+process(p_in_host_clk)
 begin
+if rising_edge(p_in_host_clk) then
   if p_in_rst = '1' then
     i_cfg_adr_cnt <= (others=>'0');
-  elsif rising_edge(p_in_host_clk) then
+  else
     if p_in_cfg_adr_ld = '1' then
       i_cfg_adr_cnt <= p_in_cfg_adr;
     else
@@ -98,6 +96,7 @@ begin
       end if;
     end if;
   end if;
+end if;
 end process;
 
 
@@ -105,8 +104,9 @@ end process;
 i_tmr_idx <= h_reg_ctrl(C_TMR_REG_CTRL_NUM_M_BIT downto C_TMR_REG_CTRL_NUM_L_BIT);
 
 --Запись регистров
-process(p_in_rst, p_in_host_clk)
+process(p_in_host_clk)
 begin
+if rising_edge(p_in_host_clk) then
   if p_in_rst = '1' then
     h_reg_ctrl <= (others=>'0');
     for i in 0 to C_TMR_COUNT - 1 loop
@@ -115,7 +115,7 @@ begin
 
     h_tmr_en <= (others=>'0');
 
-  elsif rising_edge(p_in_host_clk) then
+  else
     if p_in_cfg_wd = '1' then
       if    i_cfg_adr_cnt = CONV_STD_LOGIC_VECTOR(C_TMR_REG_CTRL, i_cfg_adr_cnt'length) then
         h_reg_ctrl <= p_in_cfg_txdata(h_reg_ctrl'high downto 0);
@@ -146,14 +146,16 @@ begin
       end if;
     end if;
   end if;
+end if;
 end process;
 
 --Чтение регистров
-process(p_in_rst, p_in_host_clk)
+process(p_in_host_clk)
 begin
+if rising_edge(p_in_host_clk) then
   if p_in_rst = '1' then
     p_out_cfg_rxdata <= (others=>'0');
-  elsif rising_edge(p_in_host_clk) then
+  else
     if p_in_cfg_rd = '1' then
       if    i_cfg_adr_cnt = CONV_STD_LOGIC_VECTOR(C_TMR_REG_CTRL, i_cfg_adr_cnt'length) then
           p_out_cfg_rxdata(h_tmr_en'high downto 0) <= h_tmr_en;
@@ -174,21 +176,23 @@ begin
       end if;
     end if;
   end if;
+end if;
 end process;
 
 
 gen_tmr : for i in 0 to C_TMR_COUNT - 1 generate
 begin
 
-  process(p_in_rst, p_in_tmr_clk)
+  process(p_in_tmr_clk)
   begin
+  if rising_edge(p_in_tmr_clk) then
     if p_in_rst = '1' then
       s_reg_ctrl(i) <= '0';
 
       i_tmr_cnt(i) <= (others=>'0');
       i_tmr_irq(i) <= '0';
 
-    elsif rising_edge(p_in_tmr_clk) then
+    else
 
       --Пересинхронизация
       s_reg_ctrl(i) <= h_tmr_en(i);
@@ -211,15 +215,17 @@ begin
       end if;
 
     end if;
+  end if;
   end process;
 
   --Растягиваем импульс установки прерывания
-  process(p_in_rst, p_in_tmr_clk)
+  process(p_in_tmr_clk)
   begin
+  if rising_edge(p_in_tmr_clk) then
     if p_in_rst = '1' then
       i_tmr_irq_cnt_width(i) <= (others=>'0');
       i_tmr_irq_width(i) <= '0';
-    elsif rising_edge(p_in_tmr_clk) then
+    else
       if i_tmr_irq(i) = '1' then
         i_tmr_irq_width(i) <= '1';
       elsif i_tmr_irq_cnt_width(i)(2) = '1' then
@@ -232,14 +238,13 @@ begin
         i_tmr_irq_cnt_width(i) <= i_tmr_irq_cnt_width(i) + 1;
       end if;
     end if;
+  end if;
   end process;
 
   --Пересинхронизация флага отработки таймера
-  process(p_in_rst, p_in_host_clk)
+  process(p_in_host_clk)
   begin
-    if p_in_rst = '1'then
-      hclk_tmr_irq_width(i) <= '0';
-    elsif rising_edge(p_in_host_clk) then
+    if rising_edge(p_in_host_clk) then
       hclk_tmr_irq_width(i) <= i_tmr_irq_width(i);
     end if;
   end process;
