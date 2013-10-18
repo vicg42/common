8d5919e96b786b1d6386310497d3de38290d0b4e
vicg <vicg42@gmail.com>
2013-10-18 08:35:49 +0000

    lib/eth -cleanup -add (signal dbg)
    git-svn-id: svn://10.1.7.240:3691/common@1571 244e6edd-17f5-4270-a3ef-e79531b98298

--- hw/eth/src/eth_mac_rx_64b.vhd
+++ hw/eth/src/eth_mac_rx_64b.vhd
@@ -119,7 +119,7 @@ signal tst_rxll_eof_n         : std_logic := '0';
 signal tst_rxll_src_rdy_n     : std_logic := '0';
 signal tst_rxbuf_full         : std_logic := '0';
 signal tst_rxll_rem           : std_logic_vector(p_in_rxll_rem'range) := (others => '0');
-
+signal tst_rxll_data           : std_logic_vector(p_in_rxll_data'range) := (others => '0');
 
 --MAIN
 begin
@@ -135,14 +135,17 @@ gen_dbg_on : if strcmp(G_DBG,"ON") generate
 ltstout:process(p_in_clk)
 begin
   if rising_edge(p_in_clk) then
-    tst_rxll_rem<=p_in_rxll_rem;
-    tst_rxll_sof_n<=p_in_rxll_sof_n;
-    tst_rxll_eof_n<=p_in_rxll_eof_n;
-    tst_rxll_src_rdy_n<=p_in_rxll_src_rdy_n;
-    tst_rxbuf_full<=p_in_rxbuf_full;
-    tst_fms_cs_dly<=tst_fms_cs;
+    tst_rxll_data <= p_in_rxll_data;
+    tst_rxll_rem <= p_in_rxll_rem;
+    tst_rxll_sof_n <= p_in_rxll_sof_n;
+    tst_rxll_eof_n <= p_in_rxll_eof_n;
+    tst_rxll_src_rdy_n <= p_in_rxll_src_rdy_n;
+    tst_rxbuf_full <= p_in_rxbuf_full;
+    tst_fms_cs_dly <= tst_fms_cs;
 
-    p_out_tst(0)<=OR_reduce(tst_fms_cs_dly) or tst_rxll_src_rdy_n or tst_rxll_eof_n or tst_rxll_sof_n or tst_rxbuf_full;-- or OR_reduce(tst_rxll_rem);
+    p_out_tst(0)<=OR_reduce(tst_fms_cs_dly) or
+                  tst_rxll_src_rdy_n or tst_rxll_eof_n or tst_rxll_sof_n or tst_rxbuf_full or
+                  OR_reduce(tst_rxll_rem) or OR_reduce(tst_rxll_data);
   end if;
 end process ltstout;
 
