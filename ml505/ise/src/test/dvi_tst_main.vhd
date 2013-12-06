8de65b650c0266dfefe473f6992bcbefa30f604f
vicg <vicg42@gmail.com>
2013-12-06 07:32:07 +0000

    ML505/dvi_test -up -cleanup
    git-svn-id: svn://10.1.7.240:3691/veresk_m@1612 244e6edd-17f5-4270-a3ef-e79531b98298

--- ml505/ise/src/test/dvi_tst_main.vhd
+++ ml505/ise/src/test/dvi_tst_main.vhd
@@ -56,8 +56,8 @@ G_CLK_T05us   : integer:=10#1000# -- кол-во периодов частоты порта p_in_clk
                                   -- укладывающиес_ в 1/2 периода 1us
 );
 port(
-p_out_test_led : out   std_logic;--//мигание сведодиода
-p_out_test_done: out   std_logic;--//сигнал переходи в '1' через 3 сек.
+p_out_test_led : out   std_logic;--мигание сведодиода
+p_out_test_done: out   std_logic;--сигнал переходи в '1' через 3 сек.
 
 p_out_1us      : out   std_logic;
 p_out_1ms      : out   std_logic;
@@ -85,6 +85,7 @@ G_DBG : string := "OFF";
 G_SIM : string := "OFF"
 );
 port(
+p_in_ctrl     : in    std_logic_vector(15 downto 0);
 p_out_err     : out   std_logic;
 
 --VIN
@@ -117,22 +118,23 @@ signal g_rst                 : std_logic;
 signal i_usrclk_rst          : std_logic;
 signal g_usrclk              : std_logic_vector(7 downto 0);
 
-signal i_dvi_ctrl_err        : std_logic;
-signal i_dvi_ctrl_clk        : std_logic;
-signal tst_div_ctrl_out      : std_logic_vector(31 downto 0);
+signal i_dvi_ctrl            : std_logic_vector(15 downto 0);
+signal i_dvi_err             : std_logic;
+signal i_dvi_clk             : std_logic;
+signal tst_div_out           : std_logic_vector(31 downto 0);
 
 signal i_test01_led          : std_logic;
 
 
 attribute keep : string;
-attribute keep of i_dvi_ctrl_clk : signal is "true";
+attribute keep of i_dvi_clk : signal is "true";
 
 
 --MAIN
 begin
 
 --***********************************************************
---//RESET модулей
+--RESET модулей
 --***********************************************************
 g_rst <= i_usrclk_rst or pin_in_btn_N;
 
@@ -149,19 +151,23 @@ p_in_clkopt=> (others=>'0'),
 p_in_clk   => pin_in_refclk
 );
 
-i_dvi_ctrl_clk <= g_usrclk(2);
+i_dvi_clk <= g_usrclk(2);
 
 
 --***********************************************************
 --
 --***********************************************************
+i_dvi_ctrl(0) <= '1'; --1/0 - Test pattern/Vin port
+i_dvi_ctrl(15 downto 1) <= (others=>'0');
+
 m_dvi : dvi_ctrl
 generic map(
-G_DBG => "ON",
+G_DBG => "OFF",
 G_SIM => "OFF"
 )
 port map(
-p_out_err     => i_dvi_ctrl_err,
+p_in_ctrl     => i_dvi_ctrl,
+p_out_err     => i_dvi_err,
 
 --VIN
 p_in_vdi      => (others=>'0'),
@@ -181,21 +187,21 @@ p_inout_scl   => pin_inout_dvi_scl,
 
 --Технологический
 p_in_tst      => (others=>'0'),
-p_out_tst     => tst_div_ctrl_out,
+p_out_tst     => tst_div_out,
 
 --System
-p_in_clk      => i_dvi_ctrl_clk,
+p_in_clk      => i_dvi_clk,
 p_in_rst      => g_rst
 );
 
 
---//#########################################
---//DBG
---//#########################################
-pin_out_TP(0) <= OR_reduce(tst_div_ctrl_out);
+--#########################################
+--DBG
+--#########################################
+pin_out_TP(0) <= OR_reduce(tst_div_out);
 
 pin_out_led(0) <= i_test01_led;
-pin_out_led(1) <= i_dvi_ctrl_err;
+pin_out_led(1) <= i_dvi_err;
 pin_out_led(2) <= '0';
 pin_out_led(3) <= '0';
 pin_out_led(4) <= '0';
@@ -217,7 +223,7 @@ p_out_1ms      => open,
 -------------------------------
 --System
 -------------------------------
-p_in_clk       => i_dvi_ctrl_clk,
+p_in_clk       => i_dvi_clk,
 p_in_rst       => g_rst
 );
 
