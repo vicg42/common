207df5313404f731486c1e463adca7a98720cb82
vicg42 <vicg42@gmail.com>
2014-09-08 15:02:34 +0300

    lib/video/video_out/char_screen.vhd

--- hw/video/video_out/char_screen.vhd
+++ hw/video/video_out/char_screen.vhd
@@ -52,8 +52,8 @@ architecture behavioral of char_screen is
 
 constant CI_FONT_SIZEX_MAX  : integer := 8;
 constant CI_FONT_SIZEY_MAX  : integer := 16;
-constant CI_CHAR_COUNTX_MAX : integer := 16;
-constant CI_CHAR_COUNTY_MAX : integer := 16;
+constant CI_CHAR_COUNTX_MAX : integer := 256;
+constant CI_CHAR_COUNTY_MAX : integer := 256;
 
 component ram_font is
 port (
@@ -93,12 +93,12 @@ signal i_screen_eny     : std_logic;
 signal i_screen_enx     : std_logic;
 signal i_screen_en      : std_logic;
 
-signal i_font_cntx      : unsigned(2 downto 0);
-signal i_font_cnty      : unsigned(3 downto 0);
+signal i_font_cntx      : unsigned(log2(CI_FONT_SIZEX_MAX) - 1 downto 0);
+signal i_font_cnty      : unsigned(log2(CI_FONT_SIZEY_MAX) - 1 downto 0);
 signal i_font_dout      : std_logic_vector(7 downto 0);
 
-signal i_char_cntx      : unsigned(7 downto 0);
-signal i_char_cnty      : unsigned(7 downto 0);
+signal i_char_cntx      : unsigned(log2(CI_CHAR_COUNTX_MAX) - 1 downto 0);
+signal i_char_cnty      : unsigned(log2(CI_CHAR_COUNTY_MAX) - 1 downto 0);
 signal i_char_ascii     : std_logic_vector(7 downto 0);
 
 signal i_font_ram_a     : unsigned(12 downto 0);
@@ -120,21 +120,19 @@ signal i_palette        : std_logic_vector((i_colg'length * 3) - 1 downto 0);
 signal tst_char         : std_logic_vector(i_font_dout'range) := (others => '0');
 signal tst_charen       : std_logic := '0';
 
-signal i_vd_out         : std_logic_vector(p_out_vd'range);
-signal tst_vd_out       : std_logic_vector(p_out_vd'range);
-signal tst_start        : std_logic;
-signal sr_tst_start     : std_logic_vector(0 to 1);
-signal tst_start_out    : std_logic;
-signal sr_screen_en     : std_logic_vector(0 to 1) := (others => '0');
-signal tst_char_out_mux : std_logic;
+--signal i_vd_out         : std_logic_vector(p_out_vd'range);
+--signal tst_start        : std_logic;
+
 
 --MAIN
 begin
 
-p_out_tst(31) <= OR_reduce(tst_vd_out) and tst_start_out and sr_screen_en(0) and tst_char_out_mux;
+p_out_tst(31) <= '0';--tst_start;
 p_out_tst(8) <= tst_charen;
 p_out_tst(7 downto 0) <= tst_char;
 
+
+--detecting area user text
 i_screen_eny <= '1' when (UNSIGNED(p_in_linecnt) >= TO_UNSIGNED(G_SCR_STARTY, p_in_linecnt'length))
                       and (UNSIGNED(p_in_linecnt) <= TO_UNSIGNED(G_SCR_STARTY + (G_FONT_SIZEY * G_SCR_SIZEY)
                                                                                           , p_in_linecnt'length)) else '0';
@@ -145,6 +143,8 @@ i_screen_enx <= '1' when (UNSIGNED(p_in_pixcnt) >= TO_UNSIGNED(G_SCR_STARTX, p_i
 
 i_screen_en <= p_in_pixen and i_screen_enx and i_screen_eny;
 
+
+--read user text ascii char and read font
 process(p_in_clk)
 begin
   if rising_edge(p_in_clk) then
@@ -152,7 +152,7 @@ begin
       i_font_cntx <= (others => '0');
       i_font_cnty <= (others => '0');
       i_char_cntx <= (others => '0');
-      i_char_cnty <= (others => '0'); tst_start <= '0';
+      i_char_cnty <= (others => '0'); --tst_start <= '0';
 
     else
 
@@ -160,11 +160,11 @@ begin
         i_font_cntx <= (others => '0');
         i_font_cnty <= (others => '0');
         i_char_cntx <= (others => '0');
-        i_char_cnty <= (others => '0'); tst_start <= '0';
+        i_char_cnty <= (others => '0'); --tst_start <= '0';
 
       else
 
-          if i_screen_en = '1' then  tst_start <= '1';
+          if i_screen_en = '1' then  --tst_start <= '1';
 
             if i_font_cntx = TO_UNSIGNED(G_FONT_SIZEX - 1, i_font_cntx'length) then
               i_font_cntx <= (others => '0');
@@ -203,7 +203,51 @@ begin
   end if;
 end process;
 
+i_font_ram_a_tmp <= TO_UNSIGNED(G_FONT_SIZEY, 8) * UNSIGNED(i_char_ascii(7 downto 0));
+i_font_ram_a <= i_font_ram_a_tmp(12 downto 0) + RESIZE(i_font_cnty, i_font_ram_a'length);
 
+i_text_ram_a_tmp <= TO_UNSIGNED(G_SCR_SIZEY, 8) * i_char_cnty(7 downto 0);
+i_text_ram_a <= i_text_ram_a_tmp(12 downto 0) + RESIZE(i_char_cntx, i_text_ram_a'length);
+
+i_text_ram_wr(0) <= p_in_ram_adr(11);
+i_font_ram_wr(0) <= p_in_ram_adr(10);
+
+m_ram_txt : ram_txt
+port map(
+clka  => p_in_clk                 ,
+ena   => i_text_ram_wr(0)         ,
+wea   => i_text_ram_wr            ,
+addra => p_in_ram_adr(9 downto 0) ,
+dina  => p_in_ram_din(31 downto 0),
+douta => open                     ,
+
+clkb  => p_in_clk                 ,
+enb   => '1'                      ,
+web   => (others => '0')          ,
+addrb => std_logic_vector(i_text_ram_a(11 downto 0)),
+dinb  => (others => '0')          ,
+doutb => i_char_ascii
+);
+
+m_ram_font : ram_font
+port map(
+clka  => p_in_clk                 ,
+ena   => i_font_ram_wr(0)         ,
+wea   => i_font_ram_wr            ,
+addra => p_in_ram_adr(9 downto 0) ,
+dina  => p_in_ram_din(31 downto 0),
+douta => open                     ,
+
+clkb  => p_in_clk                 ,
+enb   => '1'                      ,
+web   => (others => '0')          ,
+addrb => std_logic_vector(i_font_ram_a(11 downto 0)),
+dinb  => (others => '0')          ,
+doutb => i_font_dout
+);
+
+
+--shift register: pixel out current font char
 i_char_out_ld <= '1' when (i_screen_en = '1' and (i_font_cntx = TO_UNSIGNED(G_FONT_SIZEX - 1, i_font_cntx'length)))
                             or (p_in_hsync = '0') else '0';
 process(p_in_clk)
@@ -226,81 +270,36 @@ begin
   end if;
 end process;
 
-i_font_ram_a_tmp <= TO_UNSIGNED(G_FONT_SIZEY, 8) * UNSIGNED(i_char_ascii(7 downto 0));
-i_font_ram_a <= i_font_ram_a_tmp(12 downto 0) + RESIZE(i_font_cnty, i_font_ram_a'length);
-
-i_text_ram_a_tmp <= TO_UNSIGNED(G_SCR_SIZEY, 8) * i_char_cnty(7 downto 0);
-i_text_ram_a <= i_text_ram_a_tmp(12 downto 0) + RESIZE(i_char_cntx, i_text_ram_a'length);
-
-i_text_ram_wr(0) <= p_in_ram_adr(11);
-i_font_ram_wr(0) <= p_in_ram_adr(10);
-
-m_ram_txt : ram_txt
-port map(
-clka  => p_in_clk                 ,
-ena   => i_text_ram_wr(0)         ,
-wea   => i_text_ram_wr            ,
-addra => p_in_ram_adr(9 downto 0) ,
-dina  => p_in_ram_din(31 downto 0),
-douta => open                     ,
-
-clkb  => p_in_clk                 ,
-enb   => '1'                      ,
-web   => (others => '0')          ,
-addrb => std_logic_vector(i_text_ram_a(11 downto 0)),--i_text_ram_a(11 downto 0),--: in  std_logic_vector(8 downto 0);
-dinb  => (others => '0')          ,
-doutb => i_char_ascii
-);
-
-m_ram_font : ram_font
-port map(
-clka  => p_in_clk                 ,
-ena   => i_font_ram_wr(0)         ,
-wea   => i_font_ram_wr            ,
-addra => p_in_ram_adr(9 downto 0) ,--: in  std_logic_vector(6 downto 0);
-dina  => p_in_ram_din(31 downto 0),--: in  std_logic_vector(31 downto 0);
-douta => open                     ,
-
-clkb  => p_in_clk                 ,
-enb   => '1'                      ,
-web   => (others => '0')          ,
-addrb => std_logic_vector(i_font_ram_a(11 downto 0)),--std_logic_vector(p_in_pixcnt(11 downto 0)),--: in  std_logic_vector(8 downto 0);
-dinb  => (others => '0')          ,
-doutb => i_font_dout
-);
-
+--mux: pixel out or char out
 i_char_out_mux <= i_screen_en and sr_char_out(sr_char_out'length - 1);
 
 process(sr_char_out, p_in_vd, i_palette, i_char_out_mux)
 begin
   case (i_char_out_mux)is
-  when '0' => i_vd_out <= p_in_vd;
-  when '1' => i_vd_out <= std_logic_vector(RESIZE(UNSIGNED(i_palette), p_out_vd'length));
+  when '0' => p_out_vd <= p_in_vd;
+  when '1' => p_out_vd <= std_logic_vector(RESIZE(UNSIGNED(i_palette), p_out_vd'length));
   when others => null;
   end case;
 end process;
 
+--select color char out
 i_palette <= i_colr & i_colb & i_colg;
-i_colr <= (others => '1');
-i_colb <= (others => '1');
+i_colr <= (others => '0');
+i_colb <= (others => '0');
 i_colg <= (others => '1');
 
-p_out_vd <= i_vd_out;
 
+--###########################
 --DBG
+--###########################
 process(p_in_clk)
 begin
   if rising_edge(p_in_clk) then
     if p_in_rst = '1' then
       tst_char <= (others => '0');
       tst_charen <= '0';
-      sr_screen_en <= (others => '0');
-      tst_vd_out <= (others => '0');
-      sr_tst_start <= (others => '0');
-      tst_start_out <= '0'; tst_char_out_mux <= '0';
 
     else
-      sr_screen_en <= i_screen_en & sr_screen_en(0 to 0);
 
       if i_screen_en = '1' then
         if (i_font_cntx = (i_font_cntx'range => '0')) then
@@ -312,10 +311,7 @@ begin
       else
         tst_charen <= '0';
       end if;
-      tst_char_out_mux <= i_char_out_mux;
-      tst_vd_out <= i_vd_out;
-      sr_tst_start <= tst_start & sr_tst_start(0 to 0);
-      tst_start_out <= sr_tst_start(0) and not sr_tst_start(1);
+
     end if;
   end if;
 end process;
