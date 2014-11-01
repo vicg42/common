8c3f0a11023fc98d22287d42752ceadf51fcfc3c
vicg42 <vicg42@gmail.com>
2014-11-01 21:01:47 +0300

    lib/video/color_demosaic/bayer_main.vhd -add calc

--- hw/video/color_demosaic/bayer_main.vhd
+++ hw/video/color_demosaic/bayer_main.vhd
@@ -124,12 +124,32 @@ signal sr_dwnp_eof         : std_logic_vector(0 to 0);
 signal i_line_evod         : std_logic;
 signal i_pix_evod          : std_logic;
 
+signal i_pix02_line0_sum   : unsigned((8 + 1) - 1 downto 0) := (others => '0');
+signal i_pix02_line2_sum   : unsigned((8 + 1) - 1 downto 0) := (others => '0');
+signal i_pix02_line1_sum   : unsigned((8 + 1) - 1 downto 0) := (others => '0');
+signal i_pix1_line02_sum   : unsigned((8 + 1) - 1 downto 0) := (others => '0');
+signal i_pix1_line1        : unsigned(8 - 1 downto 0) := (others => '0');
+
+signal i_pix0202_line02_sum : unsigned((8 + 2) - 1 downto 0) := (others => '0');
+signal i_pix021_line102_sum : unsigned((8 + 2) - 1 downto 0) := (others => '0');
+signal sr_pix02_line1_sum   : unsigned(i_pix02_line1_sum'range) := (others => '0');
+signal sr_pix1_line02_sum   : unsigned(i_pix1_line02_sum'range) := (others => '0');
+signal sr_pix1_line1_sum    : unsigned(i_pix1_line1'range) := (others => '0');
+
+signal i_pix0202_line02_res : unsigned(8 - 1 downto 0) := (others => '0');
+signal i_pix021_line102_res : unsigned(8 - 1 downto 0) := (others => '0');
+signal i_pix02_line1_res    : unsigned(8 - 1 downto 0) := (others => '0');
+signal i_pix1_line02_res    : unsigned(8 - 1 downto 0) := (others => '0');
+signal i_pix1_line1_res     : unsigned(8 - 1 downto 0) := (others => '0');
+
 
 begin --architecture behavioral
 
-p_out_dwnp_data <= std_logic_vector(sr_matrix(1)(1));
-p_out_dwnp_wr <= sr_matrix_wr(0);
-p_out_dwnp_eof <= sr_dwnp_eof(0);
+p_out_dwnp_data((8 * 3) - 1 downto (8 * 2)) <= std_logic_vector(sr_matrix(1)(1));
+p_out_dwnp_data((8 * 2) - 1 downto (8 * 1)) <= std_logic_vector(sr_matrix(1)(1));
+p_out_dwnp_data((8 * 1) - 1 downto (8 * 0)) <= std_logic_vector(sr_matrix(1)(1));
+p_out_dwnp_wr <= sr_matrix_wr(sr_matrix_wr'high) and not p_in_dwnp_rdy_n;
+p_out_dwnp_eof <= sr_dwnp_eof(sr_dwnp_eof'high) and not p_in_dwnp_rdy_n;
 
 
 m_core : vfilter_core
@@ -192,13 +212,89 @@ if rising_edge(p_in_clk) then
       end loop;
       sr_matrix_wr <= (others => '0');
       sr_dwnp_eof <= (others => '0');
+
+      i_pix02_line0_sum <= (others => '0');
+      i_pix02_line2_sum <= (others => '0');
+      i_pix02_line1_sum <= (others => '0');
+      i_pix1_line02_sum <= (others => '0');
+      i_pix1_line1 <= (others => '0');
+
+      i_pix0202_line02_sum <= (others => '0');
+      i_pix021_line102_sum <= (others => '0');
+      sr_pix02_line1_sum <= (others => '0');
+      sr_pix1_line02_sum <= (others => '0');
+      sr_pix1_line1_sum <= (others => '0');
+
+      i_pix0202_line02_res <= (others => '0');
+      i_pix021_line102_res <= (others => '0');
+      i_pix02_line1_res <= (others => '0');
+      i_pix1_line02_res <= (others => '0');
+      i_pix1_line1_res <= (others => '0');
+
   else
     if p_in_dwnp_rdy_n = '0' then
       if i_matrix_wr = '1' then
-        sr_matrix <= i_matrix;
+--        sr_matrix <= i_matrix;
+      --------------------------------------------
+      --1
+      --------------------------------------------
+      i_pix02_line0_sum <= RESIZE(i_matrix(0)(0), i_pix02_line0_sum'length) + RESIZE(i_matrix(0)(2), i_pix02_line0_sum'length);
+      i_pix02_line2_sum <= RESIZE(i_matrix(2)(0), i_pix02_line2_sum'length) + RESIZE(i_matrix(2)(2), i_pix02_line2_sum'length);
+
+      i_pix02_line1_sum <= RESIZE(i_matrix(1)(0), i_pix02_line1_sum'length) + RESIZE(i_matrix(1)(2), i_pix02_line1_sum'length);
+      i_pix1_line02_sum <= RESIZE(i_matrix(0)(1), i_pix1_line02_sum'length) + RESIZE(i_matrix(2)(1), i_pix1_line02_sum'length);
+
+      i_pix1_line1 <= i_matrix(1)(1);
+
+      --------------------
+      --2
+      --------------------
+      i_pix0202_line02_sum <= RESIZE(i_pix02_line0_sum, i_pix0202_line02_sum'length) + RESIZE(i_pix02_line2_sum, i_pix0202_line02_sum'length);
+
+      i_pix021_line102_sum <= RESIZE(i_pix02_line1_sum, i_pix021_line102_sum'length) + RESIZE(i_pix1_line02_sum, i_pix021_line102_sum'length);
+
+      sr_pix02_line1_sum <= RESIZE(i_pix02_line1_sum, sr_pix02_line1_sum'length);
+
+      sr_pix1_line02_sum <= RESIZE(i_pix1_line02_sum, sr_pix1_line02_sum'length);
+
+      sr_pix1_line1_sum <= i_pix1_line1;
+
+      sr_pix(i)(1)<=sr_pix(i)(0);
+
+      --------------------
+      --3
+      --------------------
+      --X 0 X
+      --0 0 0
+      --X 0 X
+      i_pix0202_line02_res(7 downto 0) <= i_sum_pix0202_line02(9 downto 2);
+
+      --0 X 0
+      --X 0 X
+      --0 X 0
+      i_pix021_line102_res(7 downto 0) <= i_pix021_line102_sum(9 downto 2);
+
+      --0 0 0
+      --X 0 X
+      --0 0 0
+      i_pix02_line1_res(7 downto 0) <= sr_pix02_line1_sum(8 downto 1);
+
+      --0 X 0
+      --0 0 0
+      --0 X 0
+      i_pix1_line02_res(7 downto 0) <= sr_pix1_line02_sum(8 downto 1);
+
+      --0 0 0
+      --0 X 0
+      --0 0 0
+      i_pix1_line1_res(7 downto 0) <= sr_pix1_line1_sum(7 downto 0);
+
+
       end if;
-      sr_matrix_wr(0) <= i_matrix_wr;
-      sr_dwnp_eof(0) <= i_dwnp_eof;
+
+      sr_matrix_wr <= i_matrix_wr & sr_matrix_wr(0 to 1);
+      sr_dwnp_eof <= i_dwnp_eof & sr_dwnp_eof(0 to 1);
+
     end if;
   end if;
 end if;
