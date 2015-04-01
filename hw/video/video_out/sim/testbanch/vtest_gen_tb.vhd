e25f2aa0b362e5ead33bf6bf82d9e0dcbad1e9f6
vicg42 <vicg42@gmail.com>
2015-04-01 12:54:18 +0300

    lib/video/video_out/vtest_gen.vhd --add architecture 2, for debug SDI

--- hw/video/video_out/sim/testbanch/vtest_gen_tb.vhd
+++ hw/video/video_out/sim/testbanch/vtest_gen_tb.vhd
@@ -2,20 +2,23 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.reduce_pack.all;
 
 entity vtest_gen_tb is
 generic(
 G_DBG : string := "OFF";
-G_VD_WIDTH : integer := 256;
-G_VSYN_ACTIVE : std_logic := '0'
+G_VD_WIDTH : integer := 64;
+G_VSYN_ACTIVE : std_logic := '1'
 );
 port(
 p_out_vden    : out  std_logic;
 p_out_vd      : out  std_logic_vector(G_VD_WIDTH - 1 downto 0);
 p_out_vs      : out  std_logic;
-p_out_hs      : out  std_logic
+p_out_hs      : out  std_logic;
+
+p_out_tst     : out  std_logic_vector(31 downto 0)
 );
-end vtest_gen_tb;
+end entity vtest_gen_tb;
 
 architecture behavior of vtest_gen_tb is
 
@@ -52,20 +55,39 @@ p_in_rst      : in   std_logic
 );
 end component vtest_gen;
 
+for uut : vtest_gen use entity work.vtest_gen(test_gen_2);
+
 signal i_rst :  std_logic;
 signal i_clk :  std_logic;
 
 signal tst_vfr_pixcount  : unsigned(15 downto 0);
 signal tst_vfr_rowcount  : unsigned(15 downto 0);
 signal tst_vfr_cfg       : unsigned(15 downto 0);
-signal tst_vfr_synwidth  : unsigned(15 downto 0);
+signal tst_vfr_hs_width  : unsigned(15 downto 0);
+signal tst_vfr_vs_width  : unsigned(15 downto 0);
 
 signal i_video_den       : std_logic;
 signal i_video_d         : std_logic_vector(G_VD_WIDTH - 1 downto 0);
 signal i_video_vs        : std_logic;
 signal i_video_hs        : std_logic;
+signal i_video_pixen     : std_logic;
 
-begin
+signal sr_video_vs       : std_logic_vector(0 to 7);
+signal sr_video_hs       : std_logic_vector(0 to 7);
+signal i_trs             : std_logic;
+signal i_sav             : std_logic;
+signal i_eav             : std_logic;
+Type TSRbus2 is array (0 to 3) of unsigned(15 downto 0);
+signal i_sdi_txd         : TSRbus2;
+
+Type TSRbus is array (0 to 3) of unsigned(G_VD_WIDTH - 1 downto 0);
+signal sr_video_d        : TSRbus;
+signal i_linecnt_clr     : std_logic;
+signal i_linecnt_inc     : std_logic;
+signal i_linecnt         : unsigned(10 downto 0);
+
+
+begin --architecture behavior
 
 i_rst <='1', '0'after 500 ns;
 
@@ -89,8 +111,8 @@ port map(
 p_in_cfg      => std_logic_vector(tst_vfr_cfg),
 p_in_vpix     => std_logic_vector(tst_vfr_pixcount),
 p_in_vrow     => std_logic_vector(tst_vfr_rowcount),
-p_in_syn_h    => std_logic_vector(tst_vfr_synwidth),
-p_in_syn_v    => std_logic_vector(tst_vfr_synwidth),
+p_in_syn_h    => std_logic_vector(tst_vfr_hs_width),
+p_in_syn_v    => std_logic_vector(tst_vfr_vs_width),
 
 --Test Video
 p_out_vd      => i_video_d,
@@ -108,15 +130,16 @@ p_in_rst      => i_rst
 );
 
 
-tst_vfr_pixcount <= TO_UNSIGNED(5120 / (G_VD_WIDTH/8), tst_vfr_pixcount'length);
-tst_vfr_rowcount <= TO_UNSIGNED(5120, tst_vfr_rowcount'length);
+tst_vfr_pixcount <= TO_UNSIGNED(64, tst_vfr_pixcount'length);
+tst_vfr_rowcount <= TO_UNSIGNED(32, tst_vfr_rowcount'length);
 
 --3..0 --0/1/2/3/4 - 30fps/60fps/120fps/240fps/480fps/
 --7..4 --0/1/2/    - Test picture: V+H Counter/ V Counter/ H Counter/
-tst_vfr_cfg <= TO_UNSIGNED(16#00#, tst_vfr_cfg'length);
+tst_vfr_cfg <= TO_UNSIGNED(16#10#, tst_vfr_cfg'length);
 
---tst_vfr_synwidth <= TO_UNSIGNED(384, tst_vfr_synwidth'length);-- for 30fps (dwith 256)
-tst_vfr_synwidth <= TO_UNSIGNED(244, tst_vfr_synwidth'length);-- for 30fps (dwith 256)
+--tst_vfr_hs_width <= TO_UNSIGNED(384, tst_vfr_hs_width'length);-- for 30fps (dwith 256)
+tst_vfr_hs_width <= TO_UNSIGNED(18, tst_vfr_hs_width'length);-- for 30fps (dwith 256)
+tst_vfr_vs_width <= TO_UNSIGNED(4, tst_vfr_hs_width'length);-- for 30fps (dwith 256)
 
 p_out_vden <= i_video_den;
 p_out_vd   <= i_video_d;
@@ -125,5 +148,77 @@ p_out_hs   <= i_video_hs;
 
 i_video_den <= i_video_hs;
 
+process(i_rst, i_clk)
+begin
+if rising_edge(i_clk) then
+  if i_rst = '1' then
+    sr_video_vs <= (others => '0');
+    sr_video_hs <= (others => '0');
+    for i in 0 to sr_video_d'length - 1 loop
+    sr_video_d(i) <= (others => '0');
+    end loop;
 
-end;
+    i_linecnt_clr <= '0';
+    i_linecnt_inc <= '0';
+    i_linecnt <= (others => '0');
+
+  else
+    sr_video_vs <= i_video_vs & sr_video_vs(0 to 6);
+    sr_video_hs <= i_video_hs & sr_video_hs(0 to 6);
+    sr_video_d <= UNSIGNED(i_video_d) & sr_video_d(0 to 2);
+
+    i_linecnt_clr <= not sr_video_vs(3) and sr_video_vs(4);
+    i_linecnt_inc <= sr_video_hs(3) and not sr_video_hs(4);
+
+    if i_linecnt_clr = '1' then
+      i_linecnt <= TO_UNSIGNED(1, i_linecnt'length);
+    elsif i_linecnt_inc = '1' then
+      i_linecnt <= i_linecnt + 1;
+    end if;
+
+  end if;
+end if;
+end process;
+
+gen_sdi_d : for i in 0 to i_sdi_txd'length - 1 generate
+begin
+i_sdi_txd(i) <= TO_UNSIGNED(16#3FF#, i_sdi_txd(i)'length) when (UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#F0#, sr_video_hs'length)) or
+                                                              ((UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#FF#, sr_video_hs'length)) and i_video_hs = '0') else
+
+             TO_UNSIGNED(16#000#, i_sdi_txd(i)'length) when (UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#F8#, sr_video_hs'length)) or
+                                                            (UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#FC#, sr_video_hs'length)) or
+                                                            (UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#7F#, sr_video_hs'length)) or
+                                                            (UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#3F#, sr_video_hs'length)) else
+             --HSYNC (EAV)
+             TO_UNSIGNED(16#274#, i_sdi_txd(i)'length) when UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#FE#, sr_video_hs'length) and i_video_vs = '0' else
+
+             --HSYNC (SAV)
+             TO_UNSIGNED(16#200#, i_sdi_txd(i)'length) when UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#1F#, sr_video_hs'length) and i_video_vs = '0' else
+
+             --VSYNC (EAV)
+             TO_UNSIGNED(16#2D8#, i_sdi_txd(i)'length) when UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#FE#, sr_video_hs'length) and i_video_vs = '1' else
+
+             --VSYNC (SAV)
+             TO_UNSIGNED(16#2AC#, i_sdi_txd(i)'length) when UNSIGNED(sr_video_hs) = TO_UNSIGNED(16#1F#, sr_video_hs'length) and i_video_vs = '1' else
+
+             --DATA into SYNC strobe
+             TO_UNSIGNED(16#200#, i_sdi_txd(i)'length) when (sr_video_hs(3) = '1' or sr_video_vs(3) = '1') else
+
+             --Valid Data
+             sr_video_d(3)((i_sdi_txd(i)'length * (i + 1) - 1) downto (i_sdi_txd(i)'length * i));
+
+end generate gen_sdi_d;
+
+
+
+i_eav <= sr_video_hs(3) and not sr_video_hs(7);
+i_sav <= sr_video_hs(3) and not i_video_hs;
+
+p_out_tst(0) <= i_sav or i_eav or i_linecnt_clr or i_linecnt_inc or OR_reduce(i_linecnt)
+ or OR_reduce(std_logic_vector(i_sdi_txd(0)))
+  or OR_reduce(std_logic_vector(i_sdi_txd(1)))
+   or OR_reduce(std_logic_vector(i_sdi_txd(2)))
+    or OR_reduce(std_logic_vector(i_sdi_txd(3)));
+
+
+end architecture behavior;
--- hw/video/video_out/vtest_gen.vhd
+++ hw/video/video_out/vtest_gen.vhd
@@ -9,6 +9,18 @@
 --7..4 -  --0/1/2/    - Test picture: V+H Counter/ V Counter/ H Counter/
 --Vertical Counter - gradiet from left to right
 --Horizontal Counter - gradiet from top to bottom
+--
+-- architecture test_gen_1:
+-- p_in_syn_h --set count clk for horisontal sync
+-- p_in_syn_v --set count clk for vertical sync
+--
+-- until p_out_vs is valid port p_out_hs not change value!!!!
+--
+-- architecture test_gen_2:
+-- p_in_syn_h --set count clk for horisontal sync
+-- p_in_syn_v --set count line(hs+pixcount) for vertical sync
+--
+-- port p_out_vs and port p_out_hs  change value independently
 --------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -47,7 +59,7 @@ p_in_rst      : in   std_logic
 );
 end entity vtest_gen;
 
-architecture behavioral of vtest_gen is
+architecture test_gen_1 of vtest_gen is
 
 type fsm_state is (
 S_PIX,
@@ -74,7 +86,7 @@ signal sr_row_half          : std_logic_vector(0 to 1);
 signal i_row_half_edge      : std_logic;
 
 
-begin --architecture behavioral
+begin --architecture test_gen_1
 
 ------------------------------------
 --DBG
@@ -277,4 +289,247 @@ i_vd_out((i_vd(i)'length * (i + 1)) - 1 downto (i_vd(i)'length * i)) <= std_logi
 end generate gen_vd;
 
 
-end architecture behavioral;
+end architecture test_gen_1;
+
+
+
+--######################################################################################
+--
+--######################################################################################
+architecture test_gen_2 of vtest_gen is
+
+type TFsm_sync is (
+S_DOUT,
+S_SYN
+);
+
+signal i_fsm_hs_cs          : TFsm_sync;
+signal i_fsm_vs_cs          : TFsm_sync;
+signal i_cfg                : std_logic_vector(p_in_cfg'range);
+type TVData is array (0 to (G_VD_WIDTH / 8) -  1) of unsigned(7 downto 0);
+signal i_vd                 : TVData;
+signal i_pix_cnt            : unsigned(p_in_vpix'range) := (others => '0');
+signal i_row_cnt            : unsigned(p_in_vrow'range) := (others => '0');
+signal i_hs                 : std_logic := not G_VSYN_ACTIVE;
+signal i_vs                 : std_logic := not G_VSYN_ACTIVE;
+signal i_vd_out             : std_logic_vector(G_VD_WIDTH - 1 downto 0) := (others => '0');
+signal i_row_half           : std_logic;
+signal i_vrow_half_count    : unsigned(i_row_cnt'range);
+signal tst_i_fsm_hs_cs,tst_i_fsm_hs_cs_dly: unsigned(1 downto 0) := (others => '0');
+
+signal sr_hs                : std_logic := not G_VSYN_ACTIVE;
+signal sr_vs                : std_logic := not G_VSYN_ACTIVE;
+signal sr_vd_out            : std_logic_vector(G_VD_WIDTH - 1 downto 0) := (others => '0');
+signal sr_row_half          : std_logic_vector(0 to 1);
+signal i_row_half_edge      : std_logic;
+
+
+begin --architecture test_gen_2
+
+------------------------------------
+--DBG
+------------------------------------
+gen_dbg_off : if strcmp(G_DBG, "OFF") generate
+p_out_tst <= (others => '0');
+end generate gen_dbg_off;
+
+gen_dbg_on : if strcmp(G_DBG, "ON") generate
+p_out_tst(1 downto 0) <= std_logic_vector(tst_i_fsm_hs_cs_dly);
+p_out_tst(2) <= i_row_half;
+p_out_tst(3) <= i_row_half_edge;
+p_out_tst(31 downto 4) <= (others => '0');
+process(p_in_clk)
+begin
+  if rising_edge(p_in_clk) then
+    if p_in_clk_en = '1' then
+      tst_i_fsm_hs_cs_dly <= tst_i_fsm_hs_cs;
+      sr_row_half <= i_row_half & sr_row_half(0 to 0);
+      i_row_half_edge <= sr_row_half(0) and not sr_row_half(1);
+    end if;
+  end if;
+end process;
+--tst_i_fsm_hs_cs <= TO_UNSIGNED(16#02#,tst_i_fsm_hs_cs'length) when i_fsm_hs_cs = S_SYN_V else
+tst_i_fsm_hs_cs <= TO_UNSIGNED(16#01#,tst_i_fsm_hs_cs'length) when i_fsm_hs_cs = S_SYN else
+              TO_UNSIGNED(16#00#,tst_i_fsm_hs_cs'length);   --i_fsm_hs_cs = S_PIX      else
+end generate gen_dbg_on;
+
+
+------------------------------------
+--CFG
+------------------------------------
+process(p_in_clk)
+begin
+if rising_edge(p_in_clk) then
+  if p_in_rst = '1' then
+    i_cfg <= (others => '0');
+  else
+  if p_in_clk_en = '1' then
+      if i_vs = '1' then
+      i_cfg <= p_in_cfg;
+      end if;
+  end if;
+  end if;
+end if;--p_in_rst,
+end process;
+
+------------------------------------
+--Video
+------------------------------------
+p_out_vd <= i_vd_out;
+p_out_vs <= i_vs;
+p_out_hs <= i_hs;
+
+i_vrow_half_count <= RESIZE(UNSIGNED(p_in_vrow(p_in_vrow'length - 1 downto 1))
+                                                    , i_vrow_half_count'length);
+
+--vsync
+process(p_in_clk)
+begin
+if rising_edge(p_in_clk) then
+  if p_in_rst = '1' then
+    i_hs <= not G_VSYN_ACTIVE;
+    i_vs <= not G_VSYN_ACTIVE;
+    i_row_half <= '0';
+    i_pix_cnt <= (others => '0');
+    i_row_cnt <= (others => '0');
+    i_fsm_hs_cs <= S_DOUT;
+    i_fsm_vs_cs <= S_DOUT;
+
+  else
+  if p_in_clk_en = '1' then
+
+    --------------------------------------
+    --Horizontal synch
+    --------------------------------------
+    case i_fsm_hs_cs is
+      when S_DOUT =>
+          if i_pix_cnt = (UNSIGNED(p_in_vpix) - 1) then
+            i_pix_cnt <= (others => '0');
+            i_hs <= G_VSYN_ACTIVE;
+            i_fsm_hs_cs <= S_SYN;
+          else
+            i_pix_cnt <= i_pix_cnt + 1;
+          end if;
+
+      when S_SYN =>
+
+          if i_pix_cnt = (UNSIGNED(p_in_syn_h) - 1) then
+            i_pix_cnt <= (others => '0');
+            i_hs <= not G_VSYN_ACTIVE;
+            i_fsm_hs_cs <= S_DOUT;
+          else
+            i_pix_cnt <= i_pix_cnt + 1;
+          end if;
+
+    end case;
+
+    --------------------------------------
+    --Vertical synch
+    --------------------------------------
+    case i_fsm_vs_cs is
+      when S_DOUT =>
+          if i_pix_cnt = (UNSIGNED(p_in_vpix) - 1) then
+
+            if i_row_cnt = (UNSIGNED(p_in_vrow) - 1) then
+              i_vs <= G_VSYN_ACTIVE;
+              i_row_cnt <= (others => '0');
+              i_fsm_vs_cs <= S_SYN;
+            else
+              i_row_cnt <= i_row_cnt + 1;
+            end if;
+
+            if i_row_cnt = (i_vrow_half_count - 1) then
+              i_row_half <= '1';
+            end if;
+          end if;
+
+      when S_SYN =>
+
+          if i_pix_cnt = (UNSIGNED(p_in_vpix) - 1) then
+            if i_row_cnt = (UNSIGNED(p_in_syn_v) - 1) then
+              i_vs <= not G_VSYN_ACTIVE; i_row_half <= '0';
+              i_row_cnt <= (others => '0');
+              i_fsm_vs_cs <= S_DOUT;
+            else
+              i_row_cnt <= i_row_cnt + 1;
+            end if;
+          end if;
+
+    end case;
+
+  end if;
+  end if;
+end if;--p_in_rst,
+end process;
+
+--gen test data
+process(p_in_clk)
+begin
+if rising_edge(p_in_clk) then
+  if p_in_rst = '1' then
+    for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+    i_vd(i) <= TO_UNSIGNED(i, i_vd(i)'length);
+    end loop;
+  else
+  if p_in_clk_en = '1' then
+
+      if i_cfg(5 downto 4) = "01" then
+      --(V Counter - gradiet from left to right)
+          if i_hs = G_VSYN_ACTIVE or i_vs = G_VSYN_ACTIVE then
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= TO_UNSIGNED(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= i_vd(i) + TO_UNSIGNED((G_VD_WIDTH / 8), i_vd(i)'length);
+            end loop;
+          end if;
+
+      elsif i_cfg(5 downto 4) = "10" then
+      --(H Counter - gradiet from top to bottom)
+          if i_vs = G_VSYN_ACTIVE then
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= (others => '0');
+            end loop;
+          else
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= i_row_cnt(i_vd(i)'range);
+            end loop;
+          end if;
+
+      elsif i_cfg(5 downto 4) = "00" then
+      --(1/2 vfr - V Count; 1/2 vfr - H Count)
+        if i_row_half = '0' then
+          if i_hs = G_VSYN_ACTIVE or i_vs = G_VSYN_ACTIVE then
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= TO_UNSIGNED(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= i_vd(i) + TO_UNSIGNED((G_VD_WIDTH / 8), i_vd(i)'length);
+            end loop;
+          end if;
+        else
+          if i_vs = G_VSYN_ACTIVE then
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= TO_UNSIGNED(i, i_vd(i)'length);
+            end loop;
+          else
+            for i in 0 to (G_VD_WIDTH / 8) - 1 loop
+            i_vd(i) <= i_row_cnt(i_vd(i)'range);
+            end loop;
+          end if;
+        end if;
+      end if;
+
+  end if;
+  end if;
+end if;--p_in_rst,
+end process;
+
+gen_vd : for i in 0 to (G_VD_WIDTH / 8) - 1 generate
+i_vd_out((i_vd(i)'length * (i + 1)) - 1 downto (i_vd(i)'length * i)) <= std_logic_vector(i_vd(i));
+end generate gen_vd;
+
+
+end architecture test_gen_2;
