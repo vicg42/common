b7ac8d6f28c09434873cb6550799a7cd70b6c68a
Viktor Golovachenko <v.golovachenko@sparklab.by>
2020-05-19 21:14:50 +0300

    brightness.v

--- hw/video/filters/brightness.v
+++ hw/video/filters/brightness.v
@@ -31,12 +31,12 @@ module brightness #(
 
 localparam ZERO_FILL = (COE_WIDTH - PIXEL_WIDTH);
 localparam OVERFLOW_BIT = COE_FRACTION_WIDTH + PIXEL_WIDTH;
-reg [2:0] sr_de_i = 0;
-reg [2:0] sr_hs_i = 0;
-reg [2:0] sr_vs_i = 0;
+reg [3:0] sr_de_i = 0;
+reg [3:0] sr_hs_i = 0;
+reg [3:0] sr_vs_i = 0;
 
 wire [COE_WIDTH-1:0] di [2:0];
-reg [PIXEL_WIDTH-1:0] do_ [3-1:0];
+reg [PIXEL_WIDTH-1:0] do_ [2:0];
 genvar k;
 generate
     for (k=0; k<3; k=k+1) begin : ch
@@ -54,21 +54,31 @@ reg [(COE_WIDTH*2)-1:0] sr_br_sum = 0;
 
 reg [(COE_WIDTH*2)-1:0] mcoe_x128 = 0;
 reg [(COE_WIDTH*2)-1:0] r_m = 0;
+reg [(COE_WIDTH*2)-1:0] g_m = 0;
+reg [(COE_WIDTH*2)-1:0] b_m = 0;
 
 reg signed [(COE_WIDTH*2):0] r_m_sum = 0;
+reg signed [(COE_WIDTH*2):0] g_m_sum = 0;
+reg signed [(COE_WIDTH*2):0] b_m_sum = 0;
 
 reg signed [(COE_WIDTH*2)+1:0] r_m_sum2 = 0;
+reg signed [(COE_WIDTH*2)+1:0] g_m_sum2 = 0;
+reg signed [(COE_WIDTH*2)+1:0] b_m_sum2 = 0;
 localparam [(COE_WIDTH*2)+1:0] ROUND_ADDER = (1 << (COE_FRACTION_WIDTH - 1)); //0.5
 
 reg signed [(COE_WIDTH*2)+2:0] r_m_sum_round = 0;
+reg signed [(COE_WIDTH*2)+2:0] g_m_sum_round = 0;
+reg signed [(COE_WIDTH*2)+2:0] b_m_sum_round = 0;
 
 always @ (posedge clk) begin
     //stage0
     br_sum <= {128, {COE_FRACTION_WIDTH{1'b0}}} + {brightness, {COE_FRACTION_WIDTH{1'b0}}};
 
-    mcoe_x128 <= 128 * contrast; //{contrast[8:0], 7'd0};
+    mcoe_x128 <= contrast * 128; //{contrast[8:0], 7'd0};
 
     r_m <= contrast * di[0];
+    g_m <= contrast * di[1];
+    b_m <= contrast * di[2];
 
     sr_de_i[0] <= de_i;
     sr_hs_i[0] <= hs_i;
@@ -76,6 +86,9 @@ always @ (posedge clk) begin
 
     //stage1
     r_m_sum <= $signed({1'b0, r_m}) - $signed({1'b0, mcoe_x128});
+    g_m_sum <= $signed({1'b0, r_m}) - $signed({1'b0, mcoe_x128});
+    b_m_sum <= $signed({1'b0, r_m}) - $signed({1'b0, mcoe_x128});
+
     sr_br_sum <= br_sum;
 
     sr_de_i[1] <= sr_de_i[0];
@@ -84,22 +97,37 @@ always @ (posedge clk) begin
 
     //stage2
     r_m_sum2 <= r_m_sum + $signed({1'b0,sr_br_sum});
-
-    //stage3
-    r_m_sum_round <= r_m_sum2 + $signed(ROUND_ADDER);
+    g_m_sum2 <= g_m_sum + $signed({1'b0,sr_br_sum});
+    b_m_sum2 <= b_m_sum + $signed({1'b0,sr_br_sum});
 
     sr_de_i[2] <= sr_de_i[1];
     sr_hs_i[2] <= sr_hs_i[1];
     sr_vs_i[2] <= sr_vs_i[1];
 
     //stage3
+    r_m_sum_round <= r_m_sum2 + $signed(ROUND_ADDER);
+    g_m_sum_round <= g_m_sum2 + $signed(ROUND_ADDER);
+    b_m_sum_round <= b_m_sum2 + $signed(ROUND_ADDER);
+
+    sr_de_i[3] <= sr_de_i[2];
+    sr_hs_i[3] <= sr_hs_i[2];
+    sr_vs_i[3] <= sr_vs_i[2];
+
+    //stage3
     if (r_m_sum_round[OVERFLOW_BIT+3])                    do_[0] <= {PIXEL_WIDTH{1'b0}};
     else if (|r_m_sum_round[OVERFLOW_BIT+2:OVERFLOW_BIT]) do_[0] <= {PIXEL_WIDTH{1'b1}};
     else                                                  do_[0] <= r_m_sum_round[COE_FRACTION_WIDTH +: PIXEL_WIDTH];
 
-    de_o <= sr_de_i[2];
-    hs_o <= sr_hs_i[2];
-    vs_o <= sr_vs_i[2];
+    if (g_m_sum_round[OVERFLOW_BIT+3])                    do_[1] <= {PIXEL_WIDTH{1'b0}};
+    else if (|g_m_sum_round[OVERFLOW_BIT+2:OVERFLOW_BIT]) do_[1] <= {PIXEL_WIDTH{1'b1}};
+    else                                                  do_[1] <= g_m_sum_round[COE_FRACTION_WIDTH +: PIXEL_WIDTH];
+
+    if (b_m_sum_round[OVERFLOW_BIT+3])                    do_[2] <= {PIXEL_WIDTH{1'b0}};
+    else if (|b_m_sum_round[OVERFLOW_BIT+2:OVERFLOW_BIT]) do_[2] <= {PIXEL_WIDTH{1'b1}};
+    else                                                  do_[2] <= b_m_sum_round[COE_FRACTION_WIDTH +: PIXEL_WIDTH];
+    de_o <= sr_de_i[3];
+    hs_o <= sr_hs_i[3];
+    vs_o <= sr_vs_i[3];
 
 end
 
