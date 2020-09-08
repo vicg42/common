61f8473787553fe45bc76684493276b50d14de0e
Viktor Golovachenko <v.golovachenko@sparklab.by>
2020-09-08 17:34:57 +0300

    10b_to_8b_convert_function.py - update

--- hw/video/filters/10b_to_8b_convert_function.py
+++ hw/video/filters/10b_to_8b_convert_function.py
@@ -2,6 +2,12 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import sys
+import getopt
+import os
+
+usrfile = "width_converter_ram_default.txt"
+law = "mu"
 
 x = range(1024)
 def Discrete(x):
@@ -10,14 +16,61 @@ def Discrete(x):
 def mu_law(x, a):
     return np.log(1 + x*a) / np.log(1 + a)
 
-# u = 0.009165
-u = 0.009265
-f = [Discrete(mu_law(point, u)) for point in x]
+def line_law(x, a, b):
+    return x*a + b
 
-f0 = open("width_converter_ram_default.txt", "w")
+try:
+    options, remainder = getopt.gnu_getopt(
+        sys.argv[1:],
+        'hf:l:',
+        ['help',
+         'file=',
+         'law=',
+         ])
+except getopt.GetoptError as err:
+    print('ERROR:', err)
+    sys.exit(1)
+
+def help() :
+    print('Mandatory option: ')
+    print('\t-h   help')
+    print('\t-f   path to file. Default %s' % (usrfile) )
+    print('\t-l   law (mu, line). Default %s' % (law) )
+    print("using:")
+    print("\t %s -f <path to file> " % (os.path.basename(__file__)))
+    sys.exit()
+
+for opt, arg in options:
+    if opt in ('-f', '--file'):
+        usrfile = arg
+    elif opt in ('-l', '--law'):
+        law = arg
+    elif opt in ('-h', '--help'):
+        help()
+
+if law == "mu" :
+    u = 0.009265
+    f = [Discrete(mu_law(point, u)) for point in x]
+elif law == "line" :
+    a = 0.249
+    b = 0
+    f = [Discrete(line_law(point, a, b)) for point in x]
+else :
+    print('ERROR: bad value for -z rgument')
+    sys.exit(1)
+
+fd = open(usrfile, "w")
 for i in x :
-    print("input=0x%x  output=0x%x" % (i, Discrete(mu_law(i, u))) )
-    f0.write(hex(Discrete(mu_law(i, u)))[2:] + '\n')
+    if law == "mu" :
+        print("input=0x%x  output=0x%x" % (i, Discrete(mu_law(i, u))) )
+        fd.write(hex(Discrete(mu_law(i, u)))[2:] + '\n')
+    elif law == "line" :
+        print("input=0x%x  output=0x%x" % (i, Discrete(line_law(i, a, b))) )
+        fd.write(hex(Discrete(line_law(i, a, b)))[2:] + '\n')
+
+print("\n")
+print("Law is %s" % (law))
+print("table write to file %s" % (usrfile))
 
 plt.plot(x, f)
 plt.grid(True)
