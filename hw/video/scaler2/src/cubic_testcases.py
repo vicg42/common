768c7a465c445eaed35c6699c228a1f02c47ba80
Viktor Golovachenko <v.golovachenko@sparklab.by>
2020-10-16 11:50:56 +0300

    cleanup

--- hw/video/scaler2/src/cubic_testcases.py
+++
@@ -1,9 +0,0 @@
-import scipy.misc
-import numpy as np
-
-image = np.zeros((9, 9))
-image[4][4] = 255
-# print image
-
-print scipy.misc.imresize(image, 1.0, interp='bicubic')
-print scipy.misc.imresize(image, 1.5, interp='bicubic')
