e8394c015b9b8c22fd1e0547a16ae4e8930b966c
Viktor <vicg42@gmail.com>
2022-10-18 11:45:18 +0300

    Update bmp_io_tb.sv

--- hw/video/filters/sim/bmp_io_tb.sv
+++ hw/video/filters/sim/bmp_io_tb.sv
@@ -11,11 +11,11 @@
 
 `include "bmp_io.sv"
 
-module bmp_io_tb # (
-);
+module bmp_io_tb;
 
 BMP_IO image_real;
-BMP_IO image_new;
+BMP_IO image_gray;
+BMP_IO image_color;
 
 int pixel;
 int idx;
@@ -25,9 +25,11 @@ int w;
 int h;
 int bc;
 int bcnt;
-int image_new_w;
-int image_new_h;
-int image_new_size;
+int Green;
+int Red;
+int Blue;
+int White;
+int pixcnt;
 
 initial begin : sim_main
 
@@ -38,41 +40,65 @@ initial begin : sim_main
     y = 0;
     w = 0;
     h = 0;
-    image_new_w =0;
-    image_new_h =0;
-    image_new_size =0;
     idx = 0;
 
     //Read input image
     image_real = new();
-//    image_real.fread_bmp("img_600x600_24bit.bmp");
-    image_real.fread_bmp("bmp_io_test_in0.bmp");
+    image_real.fread_bmp("test-13.bmp");
     w = image_real.get_x();
     h = image_real.get_y();
     bc = image_real.get_ColortBitCount();
     $display("read frame: %d x %d; BItCount %d", w, h, bc);
 
-    //Create output image
-    image_new = new();
-    image_new_size = w*h*(bc/8);
-    image_new.set_pixel_array(image_new_size);
+    //Create output image gray
+    w = 256;
+    h = 256;
+    bc = 8;
+    image_gray = new();
+    image_gray.set_pixel_array(w*h*(bc/8));
     for (y = 0; y < h; y++) begin
         for (x = 0; x < w; x++) begin
-            pixel = image_real.get_pixel(x, y);
+            image_gray.set_pixel(idx, x[0 +: 8]);
+            idx++;
+        end
+    end
+    image_gray.fwrite_bmp("bmp_io_test_out_gray0.bmp", bc, w, h);
+
+    //Create output image color
+    w = 256;
+    h = 256;
+    bc = 24;
+    Green = 32'h0000FF;
+    Red = 32'h00FF00;
+    Blue = 32'hFF0000;
+    White = 32'hFFFFFF;
+    image_color = new();
+    image_color.set_pixel_array(w*h*(bc/8));
+    for (y = 0; y < h; y++) begin
+        pixcnt = 0;
+        for (x = 0; x < w; x++) begin
+            if (pixcnt[5:4] == 0) begin
+                pixel = Red;
+            end else if (pixcnt[5:4] == 1) begin
+                pixel = Green;
+            end else if (pixcnt[5:4] == 2) begin
+                pixel = Blue;
+            end else if (pixcnt[5:4] == 3) begin
+                pixel = White;
+            end
+            pixcnt++;
+
             for (bcnt = 0; bcnt < (bc/8); bcnt++) begin
-                image_new.set_pixel(idx, pixel[(bcnt*8) +: 8]);
+                image_color.set_pixel(idx, pixel[(bcnt*8) +: 8]);
                 idx++;
             end
         end
     end
 
-    image_new.fwrite_bmp("bmp_io_test_out0.bmp", bc, w, h);
+    image_color.fwrite_bmp("bmp_io_test_out_color0.bmp", bc, w, h);
 
     $stop;
 
 end : sim_main
-
-
-
 
 endmodule : bmp_io_tb
